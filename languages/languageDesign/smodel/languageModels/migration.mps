jetbrains/mps

commit 1bcbf80f9f41775612a13dd7c895b2cfb5efbf20

Refactor and optimize performance of enumeration migration.
Now helper classes do code optimization more efficiently by collecting expressions that are subject for optimization rather than traversing all expressions in a module.

+ add optimization for `equal` method calls that compare enum member raw value

languages/languageDesign/smodel/source_gen.caches/jetbrains/mps/lang/smodel/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="411zb77dnx7u7gqf7w0rx7xyphxasfu">
+<product version="3" modelHash="-3coqihqp5056hl2t4vajkxsheunx37v">
   <files names="Language.java" />
 </product>
 

languages/languageDesign/smodel/source_gen.caches/jetbrains/mps/lang/smodel/migration/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-41pu15iltgi2vk167mv6smsj06z2vcr">
-  <files names="ConceptNodeCheck.java:EnumExpressionsMigration.java:MigrateCastExpression.java:MigrateConceptSwitchToSConcept.java:MigrateDelete2Detach.java:MigrateEnumPropertyAccess.java:MigrateEnumPropertyUsages.java:MigrateModelReferenceExprWithRepo.java:MigrateOldNodePointerExpressions.java:MigrateOperationsAndNodeRefs.java:MigratePopularConceptAndNodeCasts.java:Migrate_ModelNodesOperation_concept.java:MigrationDescriptor.java:NodePointersTypesystem.java:Pattern_3qm329_a2a6.java:Pattern_3qm329_a3a6.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0a3a1a1a12.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0a3a1a1a71.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0b0b0r.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0b0b0v.java:Pattern_dd9n22_a0b0a3a0d0a3a1a1a12.java:Pattern_dd9n22_a0b0a3a0d0a3a1a1a71.java:Pattern_dd9n22_a0b0a3a0d0b0b0r.java:Pattern_dd9n22_a0b0a3a0d0b0b0v.java:Pattern_dd9n22_a0b0a3a1a1a12.java:Pattern_dd9n22_a0b0a3a1a1a71.java:Pattern_dd9n22_a0b0b0b0r.java:Pattern_dd9n22_a0b0b0b0v.java:Pattern_qvpvui_a01a6.java:Pattern_qvpvui_a02a6.java:Pattern_qvpvui_a11a6.java:Pattern_qvpvui_a12a6.java:Pattern_qvpvui_a1a6.java:Pattern_qvpvui_a21a6.java:Pattern_qvpvui_a22a6.java:Pattern_qvpvui_a2a6.java:Pattern_qvpvui_a31a6.java:Pattern_qvpvui_a32a6.java:Pattern_qvpvui_a3a6.java:Pattern_qvpvui_a42a6.java:Pattern_qvpvui_a51a6.java:Pattern_qvpvui_a52a6.java:Pattern_qvpvui_a5a6.java:Pattern_qvpvui_a61a6.java:Pattern_qvpvui_a62a6.java:Pattern_qvpvui_a6a6.java:Pattern_qvpvui_a71a6.java:Pattern_qvpvui_a72a6.java:Pattern_qvpvui_a7a6.java:Pattern_qvpvui_a8a6.java:Pattern_qvpvui_a9a6.java:Pattern_vn1hs7_a0a6.java:Pattern_vs95qi_a0a5.java:ProperIdentityInLanguageRef.java:ProperIdentityInModuleRef.java:RemoveLanguageIdVersionProperty.java:RemoveNodeConceptMethodCallDirectCallProperty.java:SNodeTypeCastExpression_completelyRemoved.java" />
+<product version="3" modelHash="-77melbymbnfvispcox0okkjvsqyhb27">
+  <files names="ConceptNodeCheck.java:EnumExpressionsMigration.java:MigrateCastExpression.java:MigrateConceptSwitchToSConcept.java:MigrateDelete2Detach.java:MigrateEnumPropertyAccess.java:MigrateEnumPropertyUsages.java:MigrateModelReferenceExprWithRepo.java:MigrateOldNodePointerExpressions.java:MigrateOperationsAndNodeRefs.java:MigratePopularConceptAndNodeCasts.java:Migrate_ModelNodesOperation_concept.java:MigrationDescriptor.java:NodePointersTypesystem.java:Pattern_3qm329_a2a6.java:Pattern_3qm329_a3a6.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0a3a1a1a22.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0a3a1a1a62.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0b0b0ab.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0b0b0w.java:Pattern_dd9n22_a0b0a3a0d0a3a1a1a22.java:Pattern_dd9n22_a0b0a3a0d0a3a1a1a62.java:Pattern_dd9n22_a0b0a3a0d0b0b0ab.java:Pattern_dd9n22_a0b0a3a0d0b0b0w.java:Pattern_dd9n22_a0b0a3a1a1a22.java:Pattern_dd9n22_a0b0a3a1a1a62.java:Pattern_dd9n22_a0b0b0b0ab.java:Pattern_dd9n22_a0b0b0b0w.java:Pattern_qvpvui_a01a6.java:Pattern_qvpvui_a02a6.java:Pattern_qvpvui_a11a6.java:Pattern_qvpvui_a12a6.java:Pattern_qvpvui_a1a6.java:Pattern_qvpvui_a21a6.java:Pattern_qvpvui_a22a6.java:Pattern_qvpvui_a2a6.java:Pattern_qvpvui_a31a6.java:Pattern_qvpvui_a32a6.java:Pattern_qvpvui_a3a6.java:Pattern_qvpvui_a42a6.java:Pattern_qvpvui_a51a6.java:Pattern_qvpvui_a52a6.java:Pattern_qvpvui_a5a6.java:Pattern_qvpvui_a61a6.java:Pattern_qvpvui_a62a6.java:Pattern_qvpvui_a6a6.java:Pattern_qvpvui_a71a6.java:Pattern_qvpvui_a72a6.java:Pattern_qvpvui_a7a6.java:Pattern_qvpvui_a8a6.java:Pattern_qvpvui_a9a6.java:Pattern_vn1hs7_a0a6.java:Pattern_vs95qi_a0a5.java:ProperIdentityInLanguageRef.java:ProperIdentityInModuleRef.java:RemoveLanguageIdVersionProperty.java:RemoveNodeConceptMethodCallDirectCallProperty.java:SNodeTypeCastExpression_completelyRemoved.java" />
 </product>
 

commit ae8367dbfd198539d420067df936f3dec4e1358c

MPS-30818 Migration on new enums breaks the code with .equals()
broken in 1bcbf80f by introducing optimization for "constant".equals(enum-value(...)).
migration causes AssertionError on `replace with` call by null receiver.

languages/languageDesign/smodel/languageModels/migration.mps
@@ -10445,43 +10445,41 @@
                                   </node>
                                 </node>
                                 <node concept="3clFbF" id="2mG04EKVPTo" role="3cqZAp">
-                                  <node concept="2OqwBi" id="2mG04EKVPTp" role="3clFbG">
-                                    <node concept="1P9Npp" id="2mG04EKVPTq" role="2OqNvi">
-                                      <node concept="2c44tf" id="2mG04EKVPTr" role="1P9ThW">
-                                        <node concept="2OqwBi" id="2mG04EKVPTs" role="2c44tc">
-                                          <node concept="1XH99k" id="2mG04EKVPTt" role="2Oq$k0">
-                                            <node concept="2c44tb" id="2mG04EKVPTu" role="lGtFl">
-                                              <property role="2qtEX8" value="enumDeclaration" />
-                                              <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/5779574625830813396/5779574625830813397" />
-                                              <node concept="1LFfDK" id="2mG04EKVPTv" role="2c44t1">
-                                                <node concept="3cmrfG" id="2mG04EKVPTw" role="1LF_Uc">
-                                                  <property role="3cmrfH" value="1" />
-                                                </node>
-                                                <node concept="37vLTw" id="2mG04EKVPTx" role="1LFl5Q">
-                                                  <ref role="3cqZAo" node="2mG04EKVPRN" resolve="valueOpMatchedL" />
-                                                </node>
-                                              </node>
-                                            </node>
-                                          </node>
-                                          <node concept="2ViDtV" id="2mG04EKVPTy" role="2OqNvi">
-                                            <node concept="2c44tb" id="2mG04EKVPTz" role="lGtFl">
-                                              <property role="2qtEX8" value="memberDeclaration" />
-                                              <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/1966870290088668512/1966870290088668516" />
-                                              <property role="3hQQBS" value="Enum_MemberLiteral" />
-                                              <node concept="37vLTw" id="2mG04EKVPT$" role="2c44t1">
-                                                <ref role="3cqZAo" node="2mG04EKVPTd" resolve="member" />
-                                              </node>
-                                            </node>
-                                          </node>
-                                        </node>
-                                      </node>
-                                    </node>
-                                    <node concept="2OqwBi" id="2mG04EKVPT_" role="2Oq$k0">
+                                  <node concept="37vLTI" id="7zN2mmXBiFh" role="3clFbG">
+                                    <node concept="2OqwBi" id="2mG04EKVPT_" role="37vLTJ">
                                       <node concept="37vLTw" id="2mG04EKWoBe" role="2Oq$k0">
                                         <ref role="3cqZAo" node="2mG04EKWog3" resolve="replacement" />
                                       </node>
                                       <node concept="3TrEf2" id="2mG04EKWU4J" role="2OqNvi">
                                         <ref role="3Tt5mk" to="tpee:fJuHU4r" resolve="rightExpression" />
+                                      </node>
+                                    </node>
+                                    <node concept="2c44tf" id="2mG04EKVPTr" role="37vLTx">
+                                      <node concept="2OqwBi" id="2mG04EKVPTs" role="2c44tc">
+                                        <node concept="1XH99k" id="2mG04EKVPTt" role="2Oq$k0">
+                                          <node concept="2c44tb" id="2mG04EKVPTu" role="lGtFl">
+                                            <property role="2qtEX8" value="enumDeclaration" />
+                                            <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/5779574625830813396/5779574625830813397" />
+                                            <node concept="1LFfDK" id="2mG04EKVPTv" role="2c44t1">
+                                              <node concept="3cmrfG" id="2mG04EKVPTw" role="1LF_Uc">
+                                                <property role="3cmrfH" value="1" />
+                                              </node>
+                                              <node concept="37vLTw" id="2mG04EKVPTx" role="1LFl5Q">
+                                                <ref role="3cqZAo" node="2mG04EKVPRN" resolve="valueOpMatchedL" />
+                                              </node>
+                                            </node>
+                                          </node>
+                                        </node>
+                                        <node concept="2ViDtV" id="2mG04EKVPTy" role="2OqNvi">
+                                          <node concept="2c44tb" id="2mG04EKVPTz" role="lGtFl">
+                                            <property role="2qtEX8" value="memberDeclaration" />
+                                            <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/1966870290088668512/1966870290088668516" />
+                                            <property role="3hQQBS" value="Enum_MemberLiteral" />
+                                            <node concept="37vLTw" id="2mG04EKVPT$" role="2c44t1">
+                                              <ref role="3cqZAo" node="2mG04EKVPTd" resolve="member" />
+                                            </node>
+                                          </node>
+                                        </node>
                                       </node>
                                     </node>
                                   </node>
@@ -10549,43 +10547,41 @@
                                   </node>
                                 </node>
                                 <node concept="3clFbF" id="2mG04EKVPSN" role="3cqZAp">
-                                  <node concept="2OqwBi" id="2mG04EKVPSO" role="3clFbG">
-                                    <node concept="1P9Npp" id="2mG04EKVPSP" role="2OqNvi">
-                                      <node concept="2c44tf" id="2mG04EKVPSQ" role="1P9ThW">
-                                        <node concept="2OqwBi" id="2mG04EKVPSR" role="2c44tc">
-                                          <node concept="1XH99k" id="2mG04EKVPSS" role="2Oq$k0">
-                                            <node concept="2c44tb" id="2mG04EKVPST" role="lGtFl">
-                                              <property role="2qtEX8" value="enumDeclaration" />
-                                              <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/5779574625830813396/5779574625830813397" />
-                                              <node concept="1LFfDK" id="2mG04EKVPSU" role="2c44t1">
-                                                <node concept="3cmrfG" id="2mG04EKVPSV" role="1LF_Uc">
-                                                  <property role="3cmrfH" value="1" />
-                                                </node>
-                                                <node concept="37vLTw" id="2mG04EKVPSW" role="1LFl5Q">
-                                                  <ref role="3cqZAo" node="2mG04EKVPRE" resolve="valueOpMatchedR" />
-                                                </node>
-                                              </node>
-                                            </node>
-                                          </node>
-                                          <node concept="2ViDtV" id="2mG04EKVPSX" role="2OqNvi">
-                                            <node concept="2c44tb" id="2mG04EKVPSY" role="lGtFl">
-                                              <property role="2qtEX8" value="memberDeclaration" />
-                                              <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/1966870290088668512/1966870290088668516" />
-                                              <property role="3hQQBS" value="Enum_MemberLiteral" />
-                                              <node concept="37vLTw" id="2mG04EKVPSZ" role="2c44t1">
-                                                <ref role="3cqZAo" node="2mG04EKVPSw" resolve="member" />
-                                              </node>
-                                            </node>
-                                          </node>
-                                        </node>
-                                      </node>
-                                    </node>
-                                    <node concept="2OqwBi" id="2mG04EKVPT0" role="2Oq$k0">
+                                  <node concept="37vLTI" id="7zN2mmXBhVH" role="3clFbG">
+                                    <node concept="2OqwBi" id="2mG04EKVPT0" role="37vLTJ">
                                       <node concept="37vLTw" id="2mG04EKWo7M" role="2Oq$k0">
                                         <ref role="3cqZAo" node="2mG04EKWnqQ" resolve="replacement" />
                                       </node>
                                       <node concept="3TrEf2" id="2mG04EKWUkT" role="2OqNvi">
                                         <ref role="3Tt5mk" to="tpee:fJuHU4s" resolve="leftExpression" />
+                                      </node>
+                                    </node>
+                                    <node concept="2c44tf" id="2mG04EKVPSQ" role="37vLTx">
+                                      <node concept="2OqwBi" id="2mG04EKVPSR" role="2c44tc">
+                                        <node concept="1XH99k" id="2mG04EKVPSS" role="2Oq$k0">
+                                          <node concept="2c44tb" id="2mG04EKVPST" role="lGtFl">
+                                            <property role="2qtEX8" value="enumDeclaration" />
+                                            <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/5779574625830813396/5779574625830813397" />
+                                            <node concept="1LFfDK" id="2mG04EKVPSU" role="2c44t1">
+                                              <node concept="3cmrfG" id="2mG04EKVPSV" role="1LF_Uc">
+                                                <property role="3cmrfH" value="1" />
+                                              </node>
+                                              <node concept="37vLTw" id="2mG04EKVPSW" role="1LFl5Q">
+                                                <ref role="3cqZAo" node="2mG04EKVPRE" resolve="valueOpMatchedR" />
+                                              </node>
+                                            </node>
+                                          </node>
+                                        </node>
+                                        <node concept="2ViDtV" id="2mG04EKVPSX" role="2OqNvi">
+                                          <node concept="2c44tb" id="2mG04EKVPSY" role="lGtFl">
+                                            <property role="2qtEX8" value="memberDeclaration" />
+                                            <property role="P3scX" value="7866978e-a0f0-4cc7-81bc-4d213d9375e1/1966870290088668512/1966870290088668516" />
+                                            <property role="3hQQBS" value="Enum_MemberLiteral" />
+                                            <node concept="37vLTw" id="2mG04EKVPSZ" role="2c44t1">
+                                              <ref role="3cqZAo" node="2mG04EKVPSw" resolve="member" />
+                                            </node>
+                                          </node>
+                                        </node>
                                       </node>
                                     </node>
                                   </node>

languages/languageDesign/smodel/source_gen.caches/jetbrains/mps/lang/smodel/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-ahk0ei1d4qs2t8y7zk58iezcd2yp040">
+<product version="3" modelHash="a33uin3wi9my34bq17wtcnyellx7156">
   <files names="Language.java" />
 </product>
 

languages/languageDesign/smodel/source_gen.caches/jetbrains/mps/lang/smodel/migration/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="82g7d6odwhkl8q78rfuuwkr1u5kb0tf">
+<product version="3" modelHash="-8pazrz4iz5wl5j7gqspjr77t8gk43vr">
   <files names="ConceptNodeCheck.java:EnumExpressionsMigration.java:MigrateCastExpression.java:MigrateConceptSwitchToSConcept.java:MigrateDelete2Detach.java:MigrateEnumPropertyAccess.java:MigrateEnumPropertyUsages.java:MigrateModelReferenceExprWithRepo.java:MigrateOldNodePointerExpressions.java:MigrateOperationsAndNodeRefs.java:MigratePopularConceptAndNodeCasts.java:Migrate_ModelNodesOperation_concept.java:MigrationDescriptor.java:NodePointersTypesystem.java:Pattern_3qm329_a2a6.java:Pattern_3qm329_a3a6.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0a3a1a1a22.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0a3a1a1a62.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0b0b0ab.java:Pattern_dd9n22_a0b0a3a0d0a3a0d0b0b0w.java:Pattern_dd9n22_a0b0a3a0d0a3a1a1a22.java:Pattern_dd9n22_a0b0a3a0d0a3a1a1a62.java:Pattern_dd9n22_a0b0a3a0d0b0b0ab.java:Pattern_dd9n22_a0b0a3a0d0b0b0w.java:Pattern_dd9n22_a0b0a3a1a1a22.java:Pattern_dd9n22_a0b0a3a1a1a62.java:Pattern_dd9n22_a0b0b0b0ab.java:Pattern_dd9n22_a0b0b0b0w.java:Pattern_qvpvui_a01a6.java:Pattern_qvpvui_a02a6.java:Pattern_qvpvui_a11a6.java:Pattern_qvpvui_a12a6.java:Pattern_qvpvui_a1a6.java:Pattern_qvpvui_a21a6.java:Pattern_qvpvui_a22a6.java:Pattern_qvpvui_a2a6.java:Pattern_qvpvui_a31a6.java:Pattern_qvpvui_a32a6.java:Pattern_qvpvui_a3a6.java:Pattern_qvpvui_a42a6.java:Pattern_qvpvui_a51a6.java:Pattern_qvpvui_a52a6.java:Pattern_qvpvui_a5a6.java:Pattern_qvpvui_a61a6.java:Pattern_qvpvui_a62a6.java:Pattern_qvpvui_a6a6.java:Pattern_qvpvui_a71a6.java:Pattern_qvpvui_a72a6.java:Pattern_qvpvui_a7a6.java:Pattern_qvpvui_a8a6.java:Pattern_qvpvui_a9a6.java:Pattern_vn1hs7_a0a6.java:Pattern_vs95qi_a0a5.java:ProperIdentityInLanguageRef.java:ProperIdentityInModuleRef.java:RemoveLanguageIdVersionProperty.java:RemoveNodeConceptMethodCallDirectCallProperty.java:SNodeTypeCastExpression_completelyRemoved.java" />
 </product>
 

languages/languageDesign/smodel/source_gen/jetbrains/mps/lang/smodel/migration/EnumExpressionsMigration.java
@@ -156,7 +156,7 @@ public class EnumExpressionsMigration extends EnumUsagesMigration {
               if (member != null) {
                 SNode replacement = getReplacementForEqualsMethodCall(imco);
                 SLinkOperations.setTarget(replacement, LINKS.leftExpression$rxLZ, valueOpMatchedL._0());
-                SNodeOperations.replaceWithAnother(SLinkOperations.getTarget(replacement, LINKS.rightExpression$rxBl), _quotation_createNode_dd9n22_a0a2a1a3a2a3a6a0a01(valueOpMatchedL._1(), member));
+                SLinkOperations.setTarget(replacement, LINKS.rightExpression$rxBl, _quotation_createNode_dd9n22_a0c0b0d0c0d0g0a0k(valueOpMatchedL._1(), member));
                 continue;
               }
             }
@@ -164,7 +164,7 @@ public class EnumExpressionsMigration extends EnumUsagesMigration {
               SNode member = matchConstant(IOperation__BehaviorDescriptor.getOperand_idhEwIP$m.invoke(imco), valueOpMatchedR._1());
               if (member != null) {
                 SNode replacement = getReplacementForEqualsMethodCall(imco);
-                SNodeOperations.replaceWithAnother(SLinkOperations.getTarget(replacement, LINKS.leftExpression$rxLZ), _quotation_createNode_dd9n22_a0a1a1a4a2a3a6a0a01(valueOpMatchedR._1(), member));
+                SLinkOperations.setTarget(replacement, LINKS.leftExpression$rxLZ, _quotation_createNode_dd9n22_a0b0b0e0c0d0g0a0k(valueOpMatchedR._1(), member));
                 SLinkOperations.setTarget(replacement, LINKS.rightExpression$rxBl, valueOpMatchedR._0());
                 continue;
               }
@@ -650,7 +650,7 @@ public class EnumExpressionsMigration extends EnumUsagesMigration {
     quotedNode_3.addChild(MetaAdapterFactory.getContainmentLink(0xf3061a5392264cc5L, 0xa443f952ceaf5816L, 0x116b46a08c4L, 0x116b46b36c4L, "operation"), quotedNode_5);
     return quotedNode_3;
   }
-  private static SNode _quotation_createNode_dd9n22_a0a2a1a3a2a3a6a0a01(Object parameter_1, Object parameter_2) {
+  private static SNode _quotation_createNode_dd9n22_a0c0b0d0c0d0g0a0k(Object parameter_1, Object parameter_2) {
     PersistenceFacade facade = PersistenceFacade.getInstance();
     SNode quotedNode_3 = null;
     SNode quotedNode_4 = null;
@@ -664,7 +664,7 @@ public class EnumExpressionsMigration extends EnumUsagesMigration {
     quotedNode_3.addChild(MetaAdapterFactory.getContainmentLink(0xf3061a5392264cc5L, 0xa443f952ceaf5816L, 0x116b46a08c4L, 0x116b46b36c4L, "operation"), quotedNode_5);
     return quotedNode_3;
   }
-  private static SNode _quotation_createNode_dd9n22_a0a1a1a4a2a3a6a0a01(Object parameter_1, Object parameter_2) {
+  private static SNode _quotation_createNode_dd9n22_a0b0b0e0c0d0g0a0k(Object parameter_1, Object parameter_2) {
     PersistenceFacade facade = PersistenceFacade.getInstance();
     SNode quotedNode_3 = null;
     SNode quotedNode_4 = null;

languages/languageDesign/smodel/source_gen/jetbrains/mps/lang/smodel/migration/trace.info
@@ -77,12 +77,12 @@
       <node id="2714544996193623628" at="154,42,155,164" concept="9" />
       <node id="2714544996193764354" at="156,35,157,76" concept="9" />
       <node id="2714544996193623656" at="157,76,158,104" concept="4" />
-      <node id="2714544996193623640" at="158,104,159,199" concept="4" />
-      <node id="2714544996193623664" at="159,199,160,25" concept="3" />
+      <node id="2714544996193623640" at="158,104,159,161" concept="4" />
+      <node id="2714544996193623664" at="159,161,160,25" concept="3" />
       <node id="2714544996193623583" at="163,42,164,131" concept="9" />
       <node id="2714544996193760949" at="165,35,166,76" concept="9" />
-      <node id="2714544996193623603" at="166,76,167,198" concept="4" />
-      <node id="2714544996193623595" at="167,198,168,105" concept="4" />
+      <node id="2714544996193623603" at="166,76,167,160" concept="4" />
+      <node id="2714544996193623595" at="167,160,168,105" concept="4" />
       <node id="2714544996193623619" at="168,105,169,25" concept="3" />
       <node id="2025969025285177306" at="177,51,178,55" concept="11" />
       <node id="2025969025285177314" at="178,55,179,17" concept="3" />
@@ -307,7 +307,7 @@
       <node id="2025969025284444059" at="648,232,649,216" concept="4" />
       <node id="2025969025284444059" at="649,216,650,166" concept="4" />
       <node id="2025969025284444059" at="650,166,651,24" concept="10" />
-      <node id="2714544996193623643" at="653,113,654,63" concept="9" />
+      <node id="2714544996193623643" at="653,111,654,63" concept="9" />
       <node id="2714544996193623643" at="654,63,655,30" concept="9" />
       <node id="2714544996193623643" at="655,30,656,30" concept="9" />
       <node id="2714544996193623643" at="656,30,657,30" concept="9" />
@@ -319,7 +319,7 @@
       <node id="2714544996193623643" at="662,232,663,216" concept="4" />
       <node id="2714544996193623643" at="663,216,664,166" concept="4" />
       <node id="2714544996193623643" at="664,166,665,24" concept="10" />
-      <node id="2714544996193623606" at="667,113,668,63" concept="9" />
+      <node id="2714544996193623606" at="667,111,668,63" concept="9" />
       <node id="2714544996193623606" at="668,63,669,30" concept="9" />
       <node id="2714544996193623606" at="669,30,670,30" concept="9" />
       <node id="2714544996193623606" at="670,30,671,30" concept="9" />
@@ -945,8 +945,8 @@
       <node id="2025969025284583043" at="612,0,625,0" concept="14" trace="colllectQueryReturnExpressions#(Lorg/jetbrains/mps/openapi/model/SNode;)Ljava/util/Set;" />
       <node id="2025969025284444022" at="625,0,639,0" concept="14" trace="_quotation_createNode_dd9n22_a0a1a1a3a1a6a0a01#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
       <node id="2025969025284444059" at="639,0,653,0" concept="14" trace="_quotation_createNode_dd9n22_a0a0a1a4a1a6a0a01#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
-      <node id="2714544996193623643" at="653,0,667,0" concept="14" trace="_quotation_createNode_dd9n22_a0a2a1a3a2a3a6a0a01#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
-      <node id="2714544996193623606" at="667,0,681,0" concept="14" trace="_quotation_createNode_dd9n22_a0a1a1a4a2a3a6a0a01#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
+      <node id="2714544996193623643" at="653,0,667,0" concept="14" trace="_quotation_createNode_dd9n22_a0c0b0d0c0d0g0a0k#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
+      <node id="2714544996193623606" at="667,0,681,0" concept="14" trace="_quotation_createNode_dd9n22_a0b0b0e0c0d0g0a0k#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
       <node id="8938145142376913237" at="681,0,695,0" concept="14" trace="_quotation_createNode_dd9n22_a0a0a4a1a2a1a01#(Ljava/lang/Object;Ljava/lang/Object;)Lorg/jetbrains/mps/openapi/model/SNode;" />
       <node id="8626562047281617056" at="181,37,196,7" concept="0" />
       <node id="2025969025285263687" at="1029,0,1044,0" concept="14" trace="_quotation_createNode_dd9n22_a0a0b0a3a0d0a3a1a1a22#()Lorg/jetbrains/mps/openapi/model/SNode;" />
@@ -1350,13 +1350,13 @@
         <var name="quotedNode_4" id="2025969025284444059" />
         <var name="quotedNode_5" id="2025969025284444059" />
       </scope>
-      <scope id="2714544996193623643" at="653,113,665,24">
+      <scope id="2714544996193623643" at="653,111,665,24">
         <var name="facade" id="2714544996193623643" />
         <var name="quotedNode_3" id="2714544996193623643" />
         <var name="quotedNode_4" id="2714544996193623643" />
         <var name="quotedNode_5" id="2714544996193623643" />
       </scope>
-      <scope id="2714544996193623606" at="667,113,679,24">
+      <scope id="2714544996193623606" at="667,111,679,24">
         <var name="facade" id="2714544996193623606" />
         <var name="quotedNode_3" id="2714544996193623606" />
         <var name="quotedNode_4" id="2714544996193623606" />