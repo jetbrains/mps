d8edc00f844f5628747324527b7a7734ee6a55fd
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2019-10-21 21:00:22 +0300

    remove tests that are included both in suite and build

--- testbench/tests/ModuleTestSuite.java
+++ testbench/tests/ModuleTestSuite.java
@@ -20,24 +20,14 @@ import jetbrains.mps.tests.TestModule_closures_test;
 import jetbrains.mps.tests.TestModule_collections_trove_test;
 import jetbrains.mps.tests.TestModule_collections_unittest;
 import jetbrains.mps.tests.TestModule_internalCollections_test;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_closures_unittest;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_math_tests;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_regexp_unittest;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_tuples_test;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_unittest;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_build_tests;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_debugger_java_runtime_tests;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_generator_tests;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_lang_editor_forms_tests;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_lang_extension_tests;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_lang_pattern_test;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_lang_smodel_unittest;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_make_tests;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_smodel_test;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_testActions;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_testRead;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_traceInfo_test;
-import jetbrains.mps.tests.TestModule_jetbrains_mps_traceInfo_testWeaving;
 import jetbrains.mps.tests.TestModule_jetbrains_mps_transformation_test_inputModels;
 import jetbrains.mps.tool.environment.EnvironmentConfig;
 import jetbrains.mps.tool.environment.IdeaEnvironment;
@@ -62,25 +52,16 @@ import org.junit.runners.model.RunnerBuilder;
                         TestModule_collections_trove_test.class,
                         TestModule_collections_unittest.class,
                         TestModule_internalCollections_test.class,
-                        TestModule_jetbrains_mps_baseLanguage_closures_unittest.class,
                         TestModule_jetbrains_mps_baseLanguage_math_tests.class,
-                        TestModule_jetbrains_mps_baseLanguage_regexp_unittest.class,
                         TestModule_jetbrains_mps_baseLanguage_tuples_test.class,
-                        TestModule_jetbrains_mps_baseLanguage_unittest.class,
-                        TestModule_jetbrains_mps_build_tests.class,
-                        TestModule_jetbrains_mps_debugger_java_runtime_tests.class,
-                        TestModule_jetbrains_mps_generator_tests.class,
                         TestModule_jetbrains_mps_lang_editor_forms_tests.class,
                         TestModule_jetbrains_mps_lang_extension_tests.class,
                         TestModule_jetbrains_mps_lang_pattern_test.class,
                         TestModule_jetbrains_mps_lang_smodel_unittest.class,
                         TestModule_jetbrains_mps_make_tests.class,
-                        TestModule_jetbrains_mps_smodel_test.class,
                         // TODO: fix or remove TestModule_jetbrains_mps_testActions test
                         // TestModule_jetbrains_mps_testActions.class,
-                        TestModule_jetbrains_mps_testRead.class,
                         TestModule_jetbrains_mps_traceInfo_test.class,
-                        TestModule_jetbrains_mps_traceInfo_testWeaving.class,
                         TestModule_jetbrains_mps_transformation_test_inputModels.class
 })
 public class ModuleTestSuite extends BaseMpsSuite {
