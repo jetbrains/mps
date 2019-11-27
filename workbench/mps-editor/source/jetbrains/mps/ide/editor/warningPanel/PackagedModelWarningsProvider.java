0822f572b23d14181742dde00dd6c784a96666e6
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2019-11-27 16:33:29 +0100

    MPS-31162 warn r/o nodes from stub models

--- workbench/mps-editor/source/jetbrains/mps/ide/editor/warningPanel/PackagedModelWarningsProvider.java
+++ workbench/mps-editor/source/jetbrains/mps/ide/editor/warningPanel/PackagedModelWarningsProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2012 JetBrains s.r.o.
+ * Copyright 2003-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,22 +16,19 @@
 package jetbrains.mps.ide.editor.warningPanel;
 
 import com.intellij.openapi.project.Project;
-import org.jetbrains.mps.openapi.model.SModel;
-import org.jetbrains.mps.openapi.model.SNode;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.mps.openapi.module.SModule;
+import org.jetbrains.mps.openapi.model.SModel;
+import org.jetbrains.mps.openapi.model.SNode;
 
+// initially, here used to be check for packaged module, hence the name
 public class PackagedModelWarningsProvider implements EditorWarningsProvider {
   @Override
   @Nullable
   public WarningPanel getWarningPanel(@NotNull SNode node, @NotNull Project project) {
     SModel model = node.getModel();
-    if (model != null) {
-      SModule module = model.getModule();
-      if (module != null && module.isReadOnly()) {
-        return new WarningPanel(this, "Warning: the node is in a read-only model. Your changes won't be saved");
-      }
+    if (model != null && model.isReadOnly()) {
+      return new WarningPanel(this, "Warning: the node is in a read-only model. Your changes won't be saved");
     }
     return null;
   }
