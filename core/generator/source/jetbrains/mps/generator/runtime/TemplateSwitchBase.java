36a71ee2f3cf279c99aa9330c8d818cb93037ceb
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2020-05-08 17:29:37 +0200

    Generator: drop deprecated methods in switch rt interface

--- core/generator/source/jetbrains/mps/generator/runtime/TemplateSwitchBase.java
+++ core/generator/source/jetbrains/mps/generator/runtime/TemplateSwitchBase.java
@@ -15,11 +15,6 @@
  */
 package jetbrains.mps.generator.runtime;
 
-import jetbrains.mps.generator.impl.DefaultTemplateContext;
-import org.jetbrains.mps.openapi.model.SNode;
-
-import java.util.Collection;
-
 /**
  * Base implementation of {@link jetbrains.mps.generator.runtime.TemplateSwitchMapping} to use as superclass in generated code
  * to facilitate future API changes
@@ -27,15 +22,4 @@ import java.util.Collection;
  */
 public abstract class TemplateSwitchBase implements TemplateSwitchMapping {
 
-  @Override
-  public Collection<SNode> applyDefault(TemplateContext context) throws GenerationException {
-    // getSwitchNode() is likely not the the same one as it used to be before the change in case it's 'extending' switch (now it's ref to extending, while used to be extended),
-    // but I see no mechanism applyDefault() could have ever used the value.
-    return applyDefault(context.getEnvironment(), getSwitchNode(), context.getInputName(), context);
-  }
-
-  @Override
-  public void processNull(TemplateExecutionEnvironment environment) {
-    processNull(environment, getSwitchNode(), new DefaultTemplateContext(environment, null, null));
-  }
 }
