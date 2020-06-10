jetbrains/mps

commit 90625c9184a3a9b5348c4322df21ca1bb145d04b

fix textgen for inner classes with expliciltly set package name

languages/baseLanguage/baseLanguage/languageModels/textGen.mps
@@ -387,6 +387,7 @@
         <child id="1145404616321" name="leftExpression" index="2JrQYb" />
       </concept>
       <concept id="1212008292747" name="jetbrains.mps.lang.smodel.structure.Model_GetLongNameOperation" flags="nn" index="LkI2h" />
+      <concept id="1171310072040" name="jetbrains.mps.lang.smodel.structure.Node_GetContainingRootOperation" flags="nn" index="2Rxl7S" />
       <concept id="1171315804604" name="jetbrains.mps.lang.smodel.structure.Model_RootsOperation" flags="nn" index="2RRcyG">
         <reference id="1171315804605" name="concept" index="2RRcyH" />
       </concept>
@@ -1245,10 +1246,19 @@
           </node>
           <node concept="2OqwBi" id="2qY3fqbZziG" role="3clFbw">
             <node concept="2OqwBi" id="2qY3fqbZxSn" role="2Oq$k0">
-              <node concept="37vLTw" id="2qY3fqbZx$Z" role="2Oq$k0">
-                <ref role="3cqZAo" node="hXZAY1h" resolve="cls" />
+              <node concept="1PxgMI" id="7i7ttYSUTiy" role="2Oq$k0">
+                <property role="1BlNFB" value="true" />
+                <node concept="chp4Y" id="7i7ttYSUTjs" role="3oSUPX">
+                  <ref role="cht4Q" to="tpee:g7pOWCK" resolve="Classifier" />
+                </node>
+                <node concept="2OqwBi" id="7i7ttYSUSe2" role="1m5AlR">
+                  <node concept="37vLTw" id="2qY3fqbZx$Z" role="2Oq$k0">
+                    <ref role="3cqZAo" node="hXZAY1h" resolve="cls" />
+                  </node>
+                  <node concept="2Rxl7S" id="7i7ttYSUSDR" role="2OqNvi" />
+                </node>
               </node>
-              <node concept="3TrcHB" id="2qY3fqbZyl0" role="2OqNvi">
+              <node concept="3TrcHB" id="7i7ttYSUTof" role="2OqNvi">
                 <ref role="3TsBF5" to="tpee:2qY3fqbSpPD" resolve="packageName" />
               </node>
             </node>

languages/baseLanguage/baseLanguage/source_gen.caches/jetbrains/mps/baseLanguage/textGen/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-60ac5k1y5sbtkzmtychk1nr63bqr1nd">
+<product version="3" modelHash="8v92i4t2ppq1ahv5a9nku75rh72ew7m">
   <files names="AdditionalForLoopVariable_TextGen.java:AndExpression_TextGen.java:AnnotationInstanceExpression_TextGen.java:AnnotationInstanceValue_TextGen.java:AnnotationInstance_TextGen.java:AnnotationMethodDeclaration_TextGen.java:Annotation_TextGen.java:AnonymousClassCreator_TextGen.java:AnonymousClass_TextGen.java:ArrayAccessExpression_TextGen.java:ArrayClassExpression_TextGen.java:ArrayCloneOperation_TextGen.java:ArrayCreatorWithInitializer_TextGen.java:ArrayCreator_TextGen.java:ArrayLengthOperation_TextGen.java:ArrayLiteral_TextGen.java:ArrayType_TextGen.java:AssertStatement_TextGen.java:BaseAssignmentExpression_TextGen.java:BaseClassConceptTextGen.java:BaseLanguageTextGen.java:BitwiseAndExpression_TextGen.java:BitwiseNotExpression_TextGen.java:BitwiseOrExpression_TextGen.java:BitwiseXorExpression_TextGen.java:BlockStatement_TextGen.java:BooleanConstant_TextGen.java:BooleanType_TextGen.java:BreakStatement_TextGen.java:ByteType_TextGen.java:CastExpression_TextGen.java:CatchClause_TextGen.java:CharConstant_TextGen.java:CharType_TextGen.java:ClassConcept_TextGen.java:ClassCreator_TextGen.java:ClassifierClassExpression_TextGen.java:ClassifierType_TextGen.java:ClassifierUnitContext.java:CommentedStatement_TextGen.java:ConceptFunctionParameter_TextGen.java:ConstructorDeclaration_TextGen.java:ContextClassifiersInRoot.java:ContinueStatement_TextGen.java:DefaultClassCreator_TextGen.java:DimensionExpression_TextGen.java:DivExpression_TextGen.java:DoWhileStatement_TextGen.java:DotExpression_TextGen.java:DoubleType_TextGen.java:ElsifClause_TextGen.java:EnumClass_TextGen.java:EnumConstantDeclaration_TextGen.java:EnumConstantReference_TextGen.java:EnumValueOfExpression_TextGen.java:EnumValuesExpression_TextGen.java:EqualsExpression_TextGen.java:ExpressionStatement_TextGen.java:FieldDeclaration_TextGen.java:FieldReferenceOperation_TextGen.java:FloatType_TextGen.java:FloatingPointConstant_TextGen.java:FloatingPointFloatConstant_TextGen.java:ForStatement_TextGen.java:ForeachStatement_TextGen.java:GenericDeclarationTextGen2.java:GenericNewExpression_TextGen.java:GreaterThanExpression_TextGen.java:GreaterThanOrEqualsExpression_TextGen.java:HexIntegerLiteral_TextGen.java:IfStatement_TextGen.java:ImplicitAnnotationInstanceValue_TextGen.java:ImportEntry.java:ImportsContext.java:InstanceInitializer_TextGen.java:InstanceMethodCallOperation_TextGen.java:InstanceMethodDeclaration_TextGen.java:InstanceOfExpression_TextGen.java:IntegerConstant_TextGen.java:IntegerType_TextGen.java:Interface_TextGen.java:LessThanExpression_TextGen.java:LessThanOrEqualsExpression_TextGen.java:LocalMethodCall_TextGen.java:LocalVariableDeclarationStatement_TextGen.java:LocalVariableDeclaration_TextGen.java:LongLiteral_TextGen.java:LongType_TextGen.java:LowerBoundType_TextGen.java:MinusExpression_TextGen.java:MulExpression_TextGen.java:NestedNewExpression_TextGen.java:NotEqualsExpression_TextGen.java:NotExpression_TextGen.java:NullLiteral_TextGen.java:OrExpression_TextGen.java:ParameterDeclaration_TextGen.java:ParenthesizedExpression_TextGen.java:PlaceholderMember_TextGen.java:PlusExpression_TextGen.java:PostfixDecrementExpression_TextGen.java:PostfixIncrementExpression_TextGen.java:PrefixDecrementExpression_TextGen.java:PrefixIncrementExpression_TextGen.java:PrimitiveClassExpression_TextGen.java:QualifiedSuperMethodCall_TextGen.java:RemExpression_TextGen.java:RemarkStatement_TextGen.java:ReturnStatement_TextGen.java:ShiftLeftExpression_TextGen.java:ShiftRightExpression_TextGen.java:ShiftRightUnsignedExpression_TextGen.java:ShortType_TextGen.java:SimpleCache.java:SingleLineComment_TextGen.java:StatementCommentPart_TextGen.java:StatementList_TextGen.java:Statement_TextGen.java:StaticFieldDeclaration_TextGen.java:StaticFieldReference_TextGen.java:StaticInitializer_TextGen.java:StaticMethodCall_TextGen.java:StaticMethodDeclaration_TextGen.java:StringLiteral_TextGen.java:StringTextGen.java:StringType_TextGen.java:SuperConstructorInvocation_TextGen.java:SuperMethodCall_TextGen.java:SwitchStatement_TextGen.java:SynchronizedStatement_TextGen.java:TernaryOperatorExpression_TextGen.java:TextCommentPart_TextGen.java:TextGenAspectDescriptor.java:ThisConstructorInvocation_TextGen.java:ThisExpression_TextGen.java:ThrowStatement_TextGen.java:TryCatchStatement_TextGen.java:TryStatement_TextGen.java:TypeVariableDeclaration_TextGen.java:TypeVariableReference_TextGen.java:UnaryMinus_TextGen.java:UpperBoundType_TextGen.java:VarType_TextGen.java:VariableArityType_TextGen.java:VariableDeclaration_TextGen.java:VariableReference_TextGen.java:Visibility_TextGen.java:VoidType_TextGen.java:WhileStatement_TextGen.java:WildCardType_TextGen.java" />
 </product>
 

languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/textGen/BaseLanguageTextGen.java
@@ -271,7 +271,7 @@ public abstract class BaseLanguageTextGen {
   }
   protected static String getPackageName(SNode cls, final TextGenContext ctx) {
     final TextGenSupport tgs = new TextGenSupport(ctx);
-    if (isNotEmptyString(SPropertyOperations.getString(cls, PROPS.packageName$3uUR))) {
+    if (isNotEmptyString(SPropertyOperations.getString(SNodeOperations.as(SNodeOperations.getContainingRoot(cls), CONCEPTS.Classifier$hJ), PROPS.packageName$3uUR))) {
       return SPropertyOperations.getString(cls, PROPS.packageName$3uUR);
     }
     return SModelOperations.getModelName(SNodeOperations.getModel(cls));

languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/textGen/trace.info
@@ -175,7 +175,7 @@
       <node id="2791683072066427009" at="266,90,267,82" concept="5" />
       <node id="7204211436280011454" at="268,7,269,97" concept="12" />
       <node id="1234796511838" at="272,79,273,55" concept="11" />
-      <node id="2791683072066470356" at="274,87,275,72" concept="12" />
+      <node id="2791683072066470356" at="274,166,275,72" concept="12" />
       <node id="1234796545766" at="276,5,277,72" concept="12" />
       <node id="1236686501608" at="279,120,280,55" concept="11" />
       <node id="1237473887359" at="281,25,282,44" concept="5" />
@@ -286,7 +286,7 @@
       <scope id="7204211436280825062" at="210,38,211,13" />
       <scope id="7204211436279995315" at="228,38,229,165" />
       <scope id="3818729150937313423" at="235,45,236,126" />
-      <scope id="2791683072066456408" at="274,87,275,72" />
+      <scope id="2791683072066456408" at="274,166,275,72" />
       <scope id="1697917907976982423" at="301,89,302,55" />
       <scope id="2791683072066470332" at="304,55,305,43" />
       <scope id="4026438661936176751" at="85,178,87,16" />

languages/baseLanguage/baseLanguage/source_gen.caches/jetbrains/mps/baseLanguage/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-avttt5xsl5i099925yq383lnm9ssy33">
+<product version="3" modelHash="4e93o3lgw66f5ygju5vwxxm8q3z4im5">
   <files names="Language.java" />
 </product>
 

languages/baseLanguage/baseLanguage/source_gen.caches/jetbrains/mps/baseLanguage/structure/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="2dcrnec3nmnzd7z867zmeqdf069ezc3">
+<product version="3" modelHash="2d8t570kl9czrbbbhws0ckvsreqbmmy">
   <files names="ConceptPresentationAspectImpl.java:EnumerationDescriptor_TrimKind.java:IconContainer.java:LanguageConceptSwitch.java:StructureAspectDescriptor.java" />
 </product>
 

languages/core/core/source_gen.caches/jetbrains/mps/lang/core/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-8pfzmwrriaiyswpioiaz2f8312l3spe">
+<product version="3" modelHash="-6fcmqh1bxo5xkyxylkxsozpr808z68z">
   <files names="Language.java" />
 </product>
 

languages/core/core/source_gen.caches/jetbrains/mps/lang/core/structure/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-1bwvbmr0ytv7pi3ik0x3uklhn0g58gr">
+<product version="3" modelHash="-dw4uwir29grmjpiyxrupjy0xs33ejje">
   <files names="ConceptPresentationAspectImpl.java:EnumerationDescriptor_SideTransformSide.java:IconContainer.java:LanguageConceptSwitch.java:StructureAspectDescriptor.java" />
 </product>
 

commit 1d1d0a3aecf0aa5eb7c6ab3324962bcc8c4c06db

MPSI-7 Fixing the problem introduced in 90625c9184a3a9b5348c4322df21ca1bb145d04b

languages/baseLanguage/baseLanguage/languageModels/textGen.mps
@@ -553,9 +553,6 @@
             <property role="TrG5h" value="shortName" />
             <property role="3TUv4t" value="false" />
             <node concept="17QB3L" id="6fUw26Uh$wr" role="1tU5fm" />
-            <node concept="Xl_RD" id="6fUw26UieFX" role="33vP2m">
-              <property role="Xl_RC" value="" />
-            </node>
           </node>
         </node>
         <node concept="3cpWs8" id="6fUw26UhzZ4" role="3cqZAp">
@@ -563,9 +560,6 @@
             <property role="TrG5h" value="packageName" />
             <property role="3TUv4t" value="false" />
             <node concept="17QB3L" id="6fUw26Uh$ww" role="1tU5fm" />
-            <node concept="Xl_RD" id="6fUw26UieFQ" role="33vP2m">
-              <property role="Xl_RC" value="" />
-            </node>
           </node>
         </node>
         <node concept="3clFbJ" id="6fUw26UhzZ8" role="3cqZAp">
@@ -1234,11 +1228,20 @@
         <node concept="3clFbJ" id="2qY3fqbZwHm" role="3cqZAp">
           <node concept="3clFbS" id="2qY3fqbZwHo" role="3clFbx">
             <node concept="3cpWs6" id="2qY3fqbZ$7k" role="3cqZAp">
-              <node concept="2OqwBi" id="2qY3fqbZA7m" role="3cqZAk">
-                <node concept="37vLTw" id="2qY3fqbZ_44" role="2Oq$k0">
-                  <ref role="3cqZAo" node="hXZAY1h" resolve="cls" />
+              <node concept="2OqwBi" id="2VL3PnQCpV9" role="3cqZAk">
+                <node concept="1PxgMI" id="2VL3PnQCpVa" role="2Oq$k0">
+                  <property role="1BlNFB" value="true" />
+                  <node concept="chp4Y" id="2VL3PnQCpVb" role="3oSUPX">
+                    <ref role="cht4Q" to="tpee:g7pOWCK" resolve="Classifier" />
+                  </node>
+                  <node concept="2OqwBi" id="2VL3PnQCpVc" role="1m5AlR">
+                    <node concept="37vLTw" id="2VL3PnQCpVd" role="2Oq$k0">
+                      <ref role="3cqZAo" node="hXZAY1h" resolve="cls" />
+                    </node>
+                    <node concept="2Rxl7S" id="2VL3PnQCpVe" role="2OqNvi" />
+                  </node>
                 </node>
-                <node concept="3TrcHB" id="2qY3fqbZBZw" role="2OqNvi">
+                <node concept="3TrcHB" id="2VL3PnQCpVf" role="2OqNvi">
                   <ref role="3TsBF5" to="tpee:2qY3fqbSpPD" resolve="packageName" />
                 </node>
               </node>

languages/baseLanguage/baseLanguage/source_gen.caches/jetbrains/mps/baseLanguage/textGen/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-2c37h4dgaqxtoqy560osg3228oo23mk">
+<product version="3" modelHash="-erzep389gmjguy5x4dx8lnjfpw80q2m">
   <files names="AdditionalForLoopVariable_TextGen.java:AndExpression_TextGen.java:AnnotationInstanceExpression_TextGen.java:AnnotationInstanceValue_TextGen.java:AnnotationInstance_TextGen.java:AnnotationMethodDeclaration_TextGen.java:Annotation_TextGen.java:AnonymousClassCreator_TextGen.java:AnonymousClass_TextGen.java:ArrayAccessExpression_TextGen.java:ArrayClassExpression_TextGen.java:ArrayCloneOperation_TextGen.java:ArrayCreatorWithInitializer_TextGen.java:ArrayCreator_TextGen.java:ArrayLengthOperation_TextGen.java:ArrayLiteral_TextGen.java:ArrayType_TextGen.java:AssertStatement_TextGen.java:BaseAssignmentExpression_TextGen.java:BaseClassConceptTextGen.java:BaseLanguageTextGen.java:BinaryIntegerLiteral_TextGen.java:BitwiseAndExpression_TextGen.java:BitwiseNotExpression_TextGen.java:BitwiseOrExpression_TextGen.java:BitwiseXorExpression_TextGen.java:BlockStatement_TextGen.java:BooleanConstant_TextGen.java:BooleanType_TextGen.java:BreakStatement_TextGen.java:ByteType_TextGen.java:CastExpression_TextGen.java:CatchClause_TextGen.java:CharConstant_TextGen.java:CharType_TextGen.java:ClassConcept_TextGen.java:ClassCreator_TextGen.java:ClassifierClassExpression_TextGen.java:ClassifierType_TextGen.java:ClassifierUnitContext.java:CommentedStatement_TextGen.java:ConceptFunctionParameter_TextGen.java:ConstructorDeclaration_TextGen.java:ContextClassifiersInRoot.java:ContinueStatement_TextGen.java:DefaultClassCreator_TextGen.java:DimensionExpression_TextGen.java:DivExpression_TextGen.java:DoWhileStatement_TextGen.java:DotExpression_TextGen.java:DoubleType_TextGen.java:ElsifClause_TextGen.java:EnumClass_TextGen.java:EnumConstantDeclaration_TextGen.java:EnumConstantReference_TextGen.java:EnumValueOfExpression_TextGen.java:EnumValuesExpression_TextGen.java:EqualsExpression_TextGen.java:ExpressionStatement_TextGen.java:FieldDeclaration_TextGen.java:FieldReferenceOperation_TextGen.java:FloatType_TextGen.java:FloatingPointConstant_TextGen.java:FloatingPointFloatConstant_TextGen.java:ForStatement_TextGen.java:ForeachStatement_TextGen.java:GenericDeclarationTextGen2.java:GenericNewExpression_TextGen.java:GreaterThanExpression_TextGen.java:GreaterThanOrEqualsExpression_TextGen.java:HexIntegerLiteral_TextGen.java:IfStatement_TextGen.java:ImplicitAnnotationInstanceValue_TextGen.java:ImportEntry.java:ImportsContext.java:InstanceInitializer_TextGen.java:InstanceMethodCallOperation_TextGen.java:InstanceMethodDeclaration_TextGen.java:InstanceOfExpression_TextGen.java:IntegerConstant_TextGen.java:IntegerType_TextGen.java:Interface_TextGen.java:LessThanExpression_TextGen.java:LessThanOrEqualsExpression_TextGen.java:LocalMethodCall_TextGen.java:LocalVariableDeclarationStatement_TextGen.java:LocalVariableDeclaration_TextGen.java:LongLiteral_TextGen.java:LongType_TextGen.java:LowerBoundType_TextGen.java:MinusExpression_TextGen.java:MulExpression_TextGen.java:NestedNewExpression_TextGen.java:NotEqualsExpression_TextGen.java:NotExpression_TextGen.java:NullLiteral_TextGen.java:OctalIntegerLiteral_TextGen.java:OrExpression_TextGen.java:ParameterDeclaration_TextGen.java:ParenthesizedExpression_TextGen.java:PlaceholderMember_TextGen.java:PlusExpression_TextGen.java:PostfixDecrementExpression_TextGen.java:PostfixIncrementExpression_TextGen.java:PrefixDecrementExpression_TextGen.java:PrefixIncrementExpression_TextGen.java:PrimitiveClassExpression_TextGen.java:QualifiedSuperMethodCall_TextGen.java:RemExpression_TextGen.java:RemarkStatement_TextGen.java:ReturnStatement_TextGen.java:ShiftLeftExpression_TextGen.java:ShiftRightExpression_TextGen.java:ShiftRightUnsignedExpression_TextGen.java:ShortType_TextGen.java:SimpleCache.java:SingleLineComment_TextGen.java:StatementCommentPart_TextGen.java:StatementList_TextGen.java:Statement_TextGen.java:StaticFieldDeclaration_TextGen.java:StaticFieldReference_TextGen.java:StaticInitializer_TextGen.java:StaticMethodCall_TextGen.java:StaticMethodDeclaration_TextGen.java:StringLiteral_TextGen.java:StringTextGen.java:StringType_TextGen.java:SuperConstructorInvocation_TextGen.java:SuperMethodCall_TextGen.java:SwitchStatement_TextGen.java:SynchronizedStatement_TextGen.java:TernaryOperatorExpression_TextGen.java:TextCommentPart_TextGen.java:TextGenAspectDescriptor.java:ThisConstructorInvocation_TextGen.java:ThisExpression_TextGen.java:ThrowStatement_TextGen.java:TryCatchStatement_TextGen.java:TryStatement_TextGen.java:TypeVariableDeclaration_TextGen.java:TypeVariableReference_TextGen.java:UnaryMinus_TextGen.java:UpperBoundType_TextGen.java:VarType_TextGen.java:VariableArityType_TextGen.java:VariableDeclaration_TextGen.java:VariableReference_TextGen.java:Visibility_TextGen.java:VoidType_TextGen.java:WhileStatement_TextGen.java:WildCardType_TextGen.java" />
 </product>
 

languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/textGen/BaseLanguageTextGen.java
@@ -220,8 +220,8 @@ public abstract class BaseLanguageTextGen {
       tgs.reportError("null reference");
       return null;
     }
-    String shortName = "";
-    String packageName = "";
+    String shortName;
+    String packageName;
     if (reference instanceof DynamicReference) {
       shortName = ((jetbrains.mps.smodel.SReference) reference).getResolveInfo();
       // hack, todo: remove! 
@@ -272,7 +272,7 @@ public abstract class BaseLanguageTextGen {
   protected static String getPackageName(SNode cls, final TextGenContext ctx) {
     final TextGenSupport tgs = new TextGenSupport(ctx);
     if (isNotEmptyString(SPropertyOperations.getString(SNodeOperations.as(SNodeOperations.getContainingRoot(cls), CONCEPTS.Classifier$hJ), PROPS.packageName$3uUR))) {
-      return SPropertyOperations.getString(cls, PROPS.packageName$3uUR);
+      return SPropertyOperations.getString(SNodeOperations.as(SNodeOperations.getContainingRoot(cls), CONCEPTS.Classifier$hJ), PROPS.packageName$3uUR);
     }
     return SModelOperations.getModelName(SNodeOperations.getModel(cls));
   }

languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/textGen/trace.info
@@ -186,8 +186,8 @@
       <node id="7204211436279973925" at="217,55,218,41" concept="11" />
       <node id="7204211436279973912" at="219,28,220,40" concept="5" />
       <node id="7204211436279973942" at="220,40,221,18" concept="12" />
-      <node id="7204211436279971777" at="222,5,223,26" concept="11" />
-      <node id="7204211436279971780" at="223,26,224,28" concept="11" />
+      <node id="7204211436279971777" at="222,5,223,21" concept="11" />
+      <node id="7204211436279971780" at="223,21,224,23" concept="11" />
       <node id="7204211436279971822" at="225,48,226,81" concept="5" />
       <node id="7204211436279995310" at="226,81,227,29" concept="13" />
       <node id="7204211436280011437" at="228,38,229,165" concept="12" />
@@ -215,7 +215,7 @@
       <node id="2791683072066427009" at="266,90,267,82" concept="5" />
       <node id="7204211436280011454" at="268,7,269,97" concept="12" />
       <node id="1234796511838" at="272,79,273,55" concept="11" />
-      <node id="2791683072066470356" at="274,166,275,72" concept="12" />
+      <node id="2791683072066470356" at="274,166,275,151" concept="12" />
       <node id="1234796545766" at="276,5,277,72" concept="12" />
       <node id="1236686501608" at="279,120,280,55" concept="11" />
       <node id="1237473887359" at="281,25,282,44" concept="5" />
@@ -311,7 +311,7 @@
       <node id="7204211436279971788" at="250,10,270,5" concept="1" />
       <node id="7204211436279995314" at="227,29,249,7" concept="9" />
       <node id="5729254867409121318" at="178,0,203,0" concept="16" trace="referenceToShortName#(Lorg/jetbrains/mps/openapi/model/SReference;Ljetbrains/mps/text/rt/TextGenContext;)V" />
-      <node id="7204211436279971784" at="224,28,270,5" concept="9" />
+      <node id="7204211436279971784" at="224,23,270,5" concept="9" />
       <node id="7204211436280011419" at="216,0,272,0" concept="16" trace="getPackageAndShortName#(Lorg/jetbrains/mps/openapi/model/SReference;Ljetbrains/mps/text/rt/TextGenContext;)Ljetbrains/mps/baseLanguage/tuples/runtime/Tuples/_2;" />
       <scope id="1237463992099" at="48,33,49,29" />
       <scope id="1237463991792" at="64,31,65,27" />
@@ -326,7 +326,7 @@
       <scope id="7204211436280825062" at="210,38,211,13" />
       <scope id="7204211436279995315" at="228,38,229,165" />
       <scope id="3818729150937313423" at="235,45,236,126" />
-      <scope id="2791683072066456408" at="274,166,275,72" />
+      <scope id="2791683072066456408" at="274,166,275,151" />
       <scope id="1697917907976982423" at="301,89,302,55" />
       <scope id="2791683072066470332" at="304,55,305,43" />
       <scope id="4026438661936176751" at="85,178,87,16" />