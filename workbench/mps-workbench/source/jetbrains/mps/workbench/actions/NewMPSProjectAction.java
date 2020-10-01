9fdd3ae3730fe15d5b7cde31dd9e92f97157dda2
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2020-10-01 12:03:15 +0200

    Update actions order on welcome screen

--- workbench/mps-workbench/source/jetbrains/mps/workbench/actions/NewMPSProjectAction.java
+++ workbench/mps-workbench/source/jetbrains/mps/workbench/actions/NewMPSProjectAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2015 JetBrains s.r.o.
+ * Copyright 2003-2020 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,36 +15,27 @@
  */
 package jetbrains.mps.workbench.actions;
 
-import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.impl.welcomeScreen.NewWelcomeScreen;
 import jetbrains.mps.workbench.action.BaseAction;
 import jetbrains.mps.workbench.dialogs.project.newproject.CreateProjectWizard;
 
-import javax.swing.Icon;
 import java.util.Map;
 
 public class NewMPSProjectAction extends BaseAction implements DumbAware {
-  private static final Icon NEW_ICON_WELCOME_SCREEN = AllIcons.Welcome.CreateNewProject;
 
   public NewMPSProjectAction() {
     setExecuteOutsideCommand(true);
     setDisableOnNoProject(false);
-
-    getTemplatePresentation().setIcon(NEW_ICON_WELCOME_SCREEN);
   }
 
   @Override
   protected void doUpdate(AnActionEvent e, Map<String, Object> _params) {
     super.doUpdate(e, _params);
-    if (ActionPlaces.WELCOME_SCREEN.equals(e.getPlace())) {
-      e.getPresentation().setIcon(NEW_ICON_WELCOME_SCREEN);
-    } else {
-      e.getPresentation().setIcon(null);
-    }
+    NewWelcomeScreen.updateNewProjectIconIfWelcomeScreen(e);
   }
 
   @Override
