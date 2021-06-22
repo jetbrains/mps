e313793151518f6552ea5d721dc574ff9a6414d0
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2021-06-22 12:36:39 +0200

    evaluate scope expresssion to recognize code in 'key'
    To help dataflow analyze scope function code, hint 'key' expression is a code (e.g  to detect variable uses)

--- languages/languageDesign/constraints/legacy/source_gen/jetbrains/mps/lang/constraints/Language.java
+++ languages/languageDesign/constraints/legacy/source_gen/jetbrains/mps/lang/constraints/Language.java
@@ -10,6 +10,8 @@ import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory;
 import jetbrains.mps.smodel.runtime.ILanguageAspect;
 import jetbrains.mps.smodel.runtime.BehaviorAspectDescriptor;
 import jetbrains.mps.smodel.runtime.ConstraintsAspectDescriptor;
+import jetbrains.mps.lang.dataFlow.framework.DataFlowAspectDescriptor;
+import jetbrains.mps.lang.constraints.dataFlow.DataFlowAspectDescriptorImpl;
 import jetbrains.mps.openapi.editor.descriptor.EditorAspectDescriptor;
 import jetbrains.mps.lang.constraints.editor.EditorAspectDescriptorImpl;
 import jetbrains.mps.lang.migration.runtime.base.MigrationAspectDescriptor;
@@ -59,6 +61,9 @@ public class Language extends LanguageRuntime {
     }
     if (aspectClass == ConstraintsAspectDescriptor.class) {
       return aspectClass.cast(new jetbrains.mps.lang.constraints.constraints.ConstraintsAspectDescriptor());
+    }
+    if (aspectClass == DataFlowAspectDescriptor.class) {
+      return aspectClass.cast(new DataFlowAspectDescriptorImpl());
     }
     if (aspectClass == EditorAspectDescriptor.class) {
       return aspectClass.cast(new EditorAspectDescriptorImpl());
