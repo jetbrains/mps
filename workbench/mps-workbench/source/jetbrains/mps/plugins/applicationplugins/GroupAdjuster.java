aab47b4024b6a129dbc73198313070d2c1687552
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2021-07-05 14:12:11 +0200

    platform 212.4416.7: fix GroupAdjuster compilation
    ProjectFrameHelper should be used instead of IdeFrameImpl

--- workbench/mps-workbench/source/jetbrains/mps/plugins/applicationplugins/GroupAdjuster.java
+++ workbench/mps-workbench/source/jetbrains/mps/plugins/applicationplugins/GroupAdjuster.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2003-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.plugins.applicationplugins;
 
@@ -24,7 +12,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.wm.ex.WindowManagerEx;
-import com.intellij.openapi.wm.impl.IdeFrameImpl;
+import com.intellij.openapi.wm.impl.ProjectFrameHelper;
 import jetbrains.mps.RuntimeFlags;
 import jetbrains.mps.ide.actions.MPSActions;
 import jetbrains.mps.ide.actions.ModuleActions_ActionGroup;
@@ -108,12 +96,12 @@ public class GroupAdjuster {
   private static void setCustomizationSchemaForCurrentProjects() {
     final Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
     for (Project project : openProjects) {
-      final IdeFrameImpl frame = WindowManagerEx.getInstanceEx().getFrame(project);
-      if (frame != null) {
-        frame.updateView();
+      final ProjectFrameHelper frameHelper = WindowManagerEx.getInstanceEx().getFrameHelper(project);
+      if (frameHelper != null) {
+        frameHelper.updateView();
       }
     }
-    final IdeFrameImpl frame = WindowManagerEx.getInstanceEx().getFrame(null);
+    final ProjectFrameHelper frame = WindowManagerEx.getInstanceEx().getFrameHelper(null);
     if (frame != null) {
       frame.updateView();
     }
