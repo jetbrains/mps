54d5330fd0d5c929aad370d975199aa050ca0c4a
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2021-08-02 17:09:21 +0200

    Quick fix for ModelDataRule
    Handle MPSNodeVirtualFile and MPSModelVirtualFile manually - model should be available right from file, no need to use FS here.

--- IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/data/ModelDataRule.java
+++ IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/data/ModelDataRule.java
@@ -21,6 +21,8 @@ import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.vfs.VirtualFile;
 import jetbrains.mps.ide.project.ProjectHelper;
+import jetbrains.mps.nodefs.MPSModelVirtualFile;
+import jetbrains.mps.nodefs.MPSNodeVirtualFile;
 import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.smodel.SModelFileTracker;
 import org.jetbrains.annotations.Nullable;
@@ -37,6 +39,12 @@ public class ModelDataRule implements GetDataRule {
     final MPSProject project = ProjectHelper.fromIdeaProject(CommonDataKeys.PROJECT.getData(dataProvider));
     if (project == null) {
       return null;
+    }
+    if (virtualFile instanceof MPSNodeVirtualFile) {
+      return ((MPSNodeVirtualFile)virtualFile).getSNodePointer().getModelReference().resolve(project.getRepository());
+    }
+    if (virtualFile instanceof MPSModelVirtualFile) {
+      return ((MPSModelVirtualFile)virtualFile).getModelReference().resolve(project.getRepository());
     }
     return SModelFileTracker.getInstance(project.getRepository()).findModel(project.getFileSystem().fromVirtualFile(virtualFile));
   }
