20ec9d208dfca77a981966b7dfc827d89e1988b5
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2021-08-20 12:07:56 +0200

    MPS-33709 Bundled MPS plugins are not available: running from sources
    Plugins are not packed in jars on sources.
    Platform mechanism to load them in such cases require some structural changes in project.
    This fix helps to overcome this problem temporary while proper solution will be implemented.

--- startup/src/jetbrains/mps/Launcher.java
+++ startup/src/jetbrains/mps/Launcher.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2003-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps;
 
@@ -21,6 +9,10 @@ import com.intellij.openapi.util.SystemInfo;
 import jetbrains.mps.util.ClassPathReader;
 
 import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +32,23 @@ public class Launcher {
     System.setProperty("idea.additional.classpath", getAdditionalMPSClasspathString());
     System.setProperty("idea.platform.prefix", "Idea");
     System.setProperty("ide.new.project.model", "false"); // Temporary disable new project model in all places
+
+    // Temporary workaround for loading plugins when running MPS from sources
+    if (Files.isDirectory(Paths.get(PathManager.getHomePath(), ".mps"))) {
+      final Path pluginPath = Path.of(PathManager.getHomePath() + File.separatorChar + "plugins");
+      ArrayList<String> plugins = new ArrayList<>();
+      try (DirectoryStream<Path> ds = Files.newDirectoryStream(pluginPath)) {
+        for (Path p : ds) {
+          if (Files.isDirectory(p)) {
+            plugins.add(p.toString());
+          }
+        }
+      }
+      if (!plugins.isEmpty()) {
+        System.setProperty("plugin.path", String.join(File.pathSeparator, plugins));
+      }
+    }
+
     Main.main(args);
   }
 
