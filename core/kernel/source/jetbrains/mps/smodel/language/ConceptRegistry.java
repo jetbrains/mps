922b20cf95f425d259d2d4bc8e6e93974afd2529
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2022-01-04 12:10:26 +0100

    MPS-29778 RT access to specialized links
    Just specialization of a link's target concept at the moment.
    We can't access declaration of a link with specialization. Now there's ReferenceDescriptor/LinkDescriptor low-level instance for these, but no SReferenceLink/SContainmentLink counterparts.

--- core/kernel/source/jetbrains/mps/smodel/language/ConceptRegistry.java
+++ core/kernel/source/jetbrains/mps/smodel/language/ConceptRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2017 JetBrains s.r.o.
+ * Copyright 2003-2022 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,6 +81,11 @@ public class ConceptRegistry implements CoreComponent, LanguageRegistryListener 
   @NotNull
   public ConstraintsRegistry getConstraintsRegistry() {
     return myConstraintsRegistry;
+  }
+
+  @NotNull
+  public StructureRegistry getStructureRegistry() {
+    return myStructureRegistry;
   }
 
   @Override
