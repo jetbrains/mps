5811acfe3c3843fa36c6d207f952588335a1c99f
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2022-08-06 08:29:18 +0200

    Fix Presentation.assertNotTemplatePresentation

--- workbench/mps-ui/source/jetbrains/mps/ide/ui/dialogs/properties/roots/editors/ToggleFBModelRootKindAction.java
+++ workbench/mps-ui/source/jetbrains/mps/ide/ui/dialogs/properties/roots/editors/ToggleFBModelRootKindAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2021 JetBrains s.r.o.
+ * Copyright 2003-2022 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,6 @@ public abstract class ToggleFBModelRootKindAction extends ToggleAction implement
     super(text, description, icon);
     myTree = tree;
     myModelRootEditor = modelRootEditor;
-    getTemplatePresentation().setEnabled(true);
   }
 
   @NotNull
