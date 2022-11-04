562d877e7b8a31a6415943d9568fa8ae4115870d
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2022-11-04 14:10:29 +0200

    MPSSPRT-413 respect all affected contributors when sending out events
    There's no guarantee PluginLoader for a project (ProjectPluginManager) comes together with any PluginContributor.
    If we see PluginLoader added in a separate delta, fireAfterPluginsLoaded() sent out empty list of contributors,
    which confused RunConfigurationStateManager and left IDEA's RunManager in old state,
    without knowledge of new RunConfiguration contributions.
    Also removed unnecessary synchronize code for Delta, as it's responsibility of EventAccumulation (e.g. we don't care
    about synchronized access for a Delta copy, right?). Besides, the pattern with direct access to Delta
    fields (d.toLoad, d.toUnload) suggests this synchronization wasn't meant seriously anyway.

--- plugins/execution-languages/languages/jetbrains.mps.execution.impl/source_gen/jetbrains/mps/execution/impl/configurations/RunConfigurationsStateManager.java
+++ plugins/execution-languages/languages/jetbrains.mps.execution.impl/source_gen/jetbrains/mps/execution/impl/configurations/RunConfigurationsStateManager.java
@@ -58,7 +58,7 @@ public class RunConfigurationsStateManager implements ProjectComponent, PluginRe
   @Override
   public void afterPluginsLoaded(List<PluginContributor> contributors) {
     // this can be executed in "later", so we check that the project is not yet closed
-    if (myProject.isDisposed() || contributors.isEmpty()) {
+    if (myProject.isDisposed()) {
       return;
     }
 
@@ -68,7 +68,7 @@ public class RunConfigurationsStateManager implements ProjectComponent, PluginRe
   @Override
   public void beforePluginsUnloaded(List<PluginContributor> contributors) {
     // same here
-    if (myProject.isDisposed() || contributors.isEmpty()) {
+    if (myProject.isDisposed()) {
       return;
     }
 
