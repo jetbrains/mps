da339717a2e57bc5eb2385b5a4d2a12d7b572dee
Maros Sandor <maros.sandor@jetbrains.com>
2023-01-06 13:41:18 +0100

    22.3 Attempt to fix JUnitOutOfProcess_Test

--- testbench/testsolutions/execution-test/tests/test_gen/jetbrains/mps/execution/impl/configurations/tests/outproces/JUnitOutOfProcess_Test.java
+++ testbench/testsolutions/execution-test/tests/test_gen/jetbrains/mps/execution/impl/configurations/tests/outproces/JUnitOutOfProcess_Test.java
@@ -149,7 +149,8 @@ public class JUnitOutOfProcess_Test extends BaseTransformationTest {
     }
     public JavaRunParameters_Configuration createDefaultJavaSettings() {
       Project ideaProject = ((MPSProject) myProject).getProject();
-      JavaRunParameters javaRunParametersTuple = new JavaRunParameters("", "", "", ".", false);
+      String vmOptions = "-Djna.boot.library.path=\"" + ideaProject.getBasePath() + "/lib/jna\"";
+      JavaRunParameters javaRunParametersTuple = new JavaRunParameters("", vmOptions, "", ".", false);
       JavaRunParameters_Configuration javaRunParams = new JavaRunParameters_Configuration(ideaProject);
       javaRunParams.setJavaParameters(javaRunParametersTuple);
       return javaRunParams;
