cec37a27ab7ebd491bc058bfdb7f0699fc21232e
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-01-27 13:12:03 +0200

    MPS-33676 NotNull annotation for method that expected to give null

--- core/persistence/source/jetbrains/mps/persistence/DefaultModelPersistence.java
+++ core/persistence/source/jetbrains/mps/persistence/DefaultModelPersistence.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2022 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -293,7 +293,7 @@ public class DefaultModelPersistence implements ModelFactory, IndexAwareModelFac
     return model.getSource();
   }
 
-  @NotNull
+  @Nullable
   public DataSource getMetaInfoLocation(@NotNull SModel model) {
     return getDataLocation(model);
   }
