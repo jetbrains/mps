166eacaecb75a0e7581dc1633b1199d578512a96
Vaclav <vaclav.pech@gmail.com>
2023-02-24 11:21:59 +0100

    MPS-35418 - accept annotations with the @Field target on EnumConstantDeclaration

--- languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/check_AnnotationTarget_NonTypesystemRule.java
+++ languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/check_AnnotationTarget_NonTypesystemRule.java
@@ -9,9 +9,9 @@ import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 import jetbrains.mps.lang.typesystem.runtime.IsApplicableStatus;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.smodel.SNodePointer;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.baseLanguage.behavior.AnnotationUtil;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.errors.messageTargets.MessageTarget;
 import jetbrains.mps.errors.messageTargets.NodeMessageTarget;
 import jetbrains.mps.errors.IErrorReporter;
@@ -19,8 +19,8 @@ import org.jetbrains.mps.openapi.language.SAbstractConcept;
 import org.jetbrains.mps.openapi.language.SReferenceLink;
 import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory;
 import org.jetbrains.mps.openapi.language.SContainmentLink;
-import org.jetbrains.mps.openapi.language.SConcept;
 import org.jetbrains.mps.openapi.language.SInterfaceConcept;
+import org.jetbrains.mps.openapi.language.SConcept;
 
 public class check_AnnotationTarget_NonTypesystemRule extends AbstractNonTypesystemRule_Runtime implements NonTypesystemRule_Runtime {
   public check_AnnotationTarget_NonTypesystemRule() {
@@ -34,7 +34,7 @@ public class check_AnnotationTarget_NonTypesystemRule extends AbstractNonTypesys
         break;
       }
     }
-    if (targetAi == null || (!(SNodeOperations.isInstanceOf(SNodeOperations.getParent(annotationInstance), CONCEPTS.HasAnnotation$Dg))) && SNodeOperations.getNodeAncestor(annotationInstance, CONCEPTS.AnnotationInstance$yl, false, false) != null) {
+    if (targetAi == null || this._additional_isHookedUnderAnotherAnnotation(annotationInstance)) {
       return;
     }
     boolean goodTarget = AnnotationUtil.accept(SLinkOperations.getTarget(ListSequence.fromList(SLinkOperations.getChildren(targetAi, LINKS.value$uK2B)).first(), LINKS.value$Y7om), SNodeOperations.as(SNodeOperations.getParent(annotationInstance), CONCEPTS.HasAnnotation$Dg));
@@ -49,6 +49,9 @@ public class check_AnnotationTarget_NonTypesystemRule extends AbstractNonTypesys
   public IsApplicableStatus isApplicableAndPattern(SNode argument) {
     return new IsApplicableStatus(argument.getConcept().isSubConceptOf(getApplicableConcept()), null);
   }
+  private boolean _additional_isHookedUnderAnotherAnnotation(SNode currentAnnotationInstance) {
+    return (!(SNodeOperations.isInstanceOf(SNodeOperations.getParent(currentAnnotationInstance), CONCEPTS.HasAnnotation$Dg))) && SNodeOperations.getNodeAncestor(currentAnnotationInstance, CONCEPTS.AnnotationInstance$yl, false, false) != null;
+  }
   public boolean overrides() {
     return false;
   }
@@ -61,7 +64,7 @@ public class check_AnnotationTarget_NonTypesystemRule extends AbstractNonTypesys
   }
 
   private static final class CONCEPTS {
-    /*package*/ static final SConcept AnnotationInstance$yl = MetaAdapterFactory.getConcept(0xf3061a5392264cc5L, 0xa443f952ceaf5816L, 0x114a6b4ccabL, "jetbrains.mps.baseLanguage.structure.AnnotationInstance");
     /*package*/ static final SInterfaceConcept HasAnnotation$Dg = MetaAdapterFactory.getInterfaceConcept(0xf3061a5392264cc5L, 0xa443f952ceaf5816L, 0x114a6be947aL, "jetbrains.mps.baseLanguage.structure.HasAnnotation");
+    /*package*/ static final SConcept AnnotationInstance$yl = MetaAdapterFactory.getConcept(0xf3061a5392264cc5L, 0xa443f952ceaf5816L, 0x114a6b4ccabL, "jetbrains.mps.baseLanguage.structure.AnnotationInstance");
   }
 }
