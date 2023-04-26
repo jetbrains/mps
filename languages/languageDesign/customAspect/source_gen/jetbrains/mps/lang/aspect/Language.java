32f66944eeaddfc7ea61baca043f32ac7ba21161
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-04-26 12:00:37 +0300

    MPS-34831 Check concepts referenced in ARC are rootable
    Although I don't think 'rootable' attribute makes any sense in a ConceptDeclaration, still makes sense to
    use it for transition until we get rid of it, and the checking rule won't be necessary)

--- languages/languageDesign/customAspect/source_gen/jetbrains/mps/lang/aspect/Language.java
+++ languages/languageDesign/customAspect/source_gen/jetbrains/mps/lang/aspect/Language.java
@@ -14,6 +14,8 @@ import jetbrains.mps.lang.aspect.editor.EditorAspectDescriptorImpl;
 import jetbrains.mps.smodel.runtime.StructureAspectDescriptor;
 import jetbrains.mps.smodel.runtime.ConceptPresentationAspect;
 import jetbrains.mps.lang.aspect.structure.ConceptPresentationAspectImpl;
+import jetbrains.mps.lang.typesystem.runtime.IHelginsDescriptor;
+import jetbrains.mps.lang.aspect.typesystem.TypesystemDescriptor;
 import org.jetbrains.annotations.NotNull;
 import jetbrains.mps.smodel.language.LanguageExtensions;
 
@@ -57,6 +59,9 @@ public class Language extends LanguageRuntime {
     }
     if (aspectClass == ConceptPresentationAspect.class) {
       return aspectClass.cast(new ConceptPresentationAspectImpl());
+    }
+    if (aspectClass == IHelginsDescriptor.class) {
+      return aspectClass.cast(new TypesystemDescriptor());
     }
     return null;
   }
