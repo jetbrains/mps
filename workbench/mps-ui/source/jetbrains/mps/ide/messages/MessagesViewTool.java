c0d411f8855325dc10a0856d14f13bde7c7f8774
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-07-03 15:09:40 +0300

    Don't use deprecated MessageView.SERVICE

--- workbench/mps-ui/source/jetbrains/mps/ide/messages/MessagesViewTool.java
+++ workbench/mps-ui/source/jetbrains/mps/ide/messages/MessagesViewTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.ide.messages;
 
@@ -130,7 +130,7 @@ public class MessagesViewTool implements PersistentStateComponent<MessageViewToo
   }
 
   private MessageView getMessagesService() {
-    return MessageView.SERVICE.getInstance(myProject);
+    return MessageView.getInstance(myProject);
   }
 
   public IMessageHandler newHandler() {
