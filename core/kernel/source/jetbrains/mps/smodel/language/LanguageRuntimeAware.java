5062f8076d2e6c46986e8f1d146d83f053946e3e
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-08-02 13:33:23 +0300

    Ease discoverability of LanguageRuntimeAware interface
    As it's intended for use as a mix-in with language aspect interfaces, clearly indicate this with superinterface

--- core/kernel/source/jetbrains/mps/smodel/language/LanguageRuntimeAware.java
+++ core/kernel/source/jetbrains/mps/smodel/language/LanguageRuntimeAware.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2016 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package jetbrains.mps.smodel.language;
 
+import jetbrains.mps.smodel.runtime.ILanguageAspect;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -22,6 +23,6 @@ import org.jetbrains.annotations.NotNull;
  *
  * @see LanguageRuntime#getAspect(Class)
  */
-public interface LanguageRuntimeAware {
+public interface LanguageRuntimeAware extends ILanguageAspect {
   void setLanguageRuntime(@NotNull LanguageRuntime languageRuntime);
 }
