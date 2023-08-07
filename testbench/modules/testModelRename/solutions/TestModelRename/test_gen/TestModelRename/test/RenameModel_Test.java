d5e95a814293d534ff81694066632456e124ae99
Vaclav <vaclav.pech@gmail.com>
2023-08-07 14:13:15 +0200

    MPS-35735 - Rebuild project after merge

--- testbench/modules/testModelRename/solutions/TestModelRename/test_gen/TestModelRename/test/RenameModel_Test.java
+++ testbench/modules/testModelRename/solutions/TestModelRename/test_gen/TestModelRename/test/RenameModel_Test.java
@@ -4,9 +4,10 @@ package TestModelRename.test;
 
 import jetbrains.mps.MPSLaunch;
 import jetbrains.mps.lang.test.runtime.BaseTransformationTest;
-import org.junit.ClassRule;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import jetbrains.mps.lang.test.runtime.TestParametersCacheExtension;
 import jetbrains.mps.lang.test.runtime.TestParametersCache;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import jetbrains.mps.lang.test.runtime.BaseTestBody;
 import jetbrains.mps.lang.test.runtime.TransformationTest;
 import org.jetbrains.mps.openapi.model.SModel;
@@ -27,11 +28,11 @@ import org.jetbrains.mps.openapi.persistence.UnsupportedDataSourceException;
 
 @MPSLaunch
 public class RenameModel_Test extends BaseTransformationTest {
-  @ClassRule
-  public static final TestParametersCache ourParamCache = new TestParametersCache(RenameModel_Test.class, "${mps_home}", "r:306699ac-7981-4117-bc07-ec2d425bff05(TestModelRename.test@tests)", false);
+  @RegisterExtension
+  private static final TestParametersCacheExtension ourParametersCacheExtension = new TestParametersCacheExtension(new TestParametersCache(RenameModel_Test.class, "${mps_home}", "r:306699ac-7981-4117-bc07-ec2d425bff05(TestModelRename.test@tests)", false));
 
   public RenameModel_Test() {
-    super(ourParamCache);
+    super(ourParametersCacheExtension.getParametersCache());
   }
 
   @Test
