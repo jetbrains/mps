61a238fbefa4b3dc7f5215952ac190a9367525ba
tatiana.elfimova <tatiana.elfimova@jetbrains.com>
2023-08-15 12:22:06 +0200

    Documentation text decoration
    Get a piece of html with doc text and the name of the module at the bottom

--- editor/editor-runtime/source/jetbrains/mps/nodeEditor/MPSDocumentationManager.java
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/MPSDocumentationManager.java
@@ -7,10 +7,16 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.ComponentPopupBuilder;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.text.HtmlChunk;
 import com.intellij.ui.popup.AbstractPopup;
 import jetbrains.mps.logging.Logger;
+import jetbrains.mps.nodeEditor.documentation.MPSDocumentationMarkup;
 import jetbrains.mps.nodeEditor.documentation.ui.MPSDocumentationPopupUI;
 import jetbrains.mps.nodeEditor.documentation.ui.MPSDocumentationUI;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.mps.openapi.model.SModel;
+import org.jetbrains.mps.openapi.model.SNode;
 import java.awt.Frame;
 import java.awt.Point;
 
@@ -28,7 +34,7 @@ public class MPSDocumentationManager {
   /**
    * Show a quick documentation
    */
-  public void showQuickDocumentation(EditorComponent editorComponent, Frame owner, Project project, Point location, String doc){
+  public void showQuickDocumentation(Frame owner, Project project, Point location, String doc){
     MPSDocumentationPopupUI popupUI = new MPSDocumentationPopupUI(new MPSDocumentationUI(project, doc));
 
     ComponentPopupBuilder builder = JBPopupFactory.getInstance().createComponentPopupBuilder(popupUI.getComponent(), popupUI.getPreferableFocusComponent()).setProject(project).setResizable(true).setMovable(true).setFocusable(true).setModalContext(true);
@@ -36,4 +42,27 @@ public class MPSDocumentationManager {
     popupUI.setPopup(popup);
     popup.show(owner, location.x, location.y, true);
   }
+
+  private static HtmlChunk getModelInfo(SNode node){
+    SModel model = node.getModel();
+    return HtmlChunk.fragment(HtmlChunk.tag("icon").attr("src", "jetbrains.mps.icons.MPSIcons.Nodes.Model"),
+                              HtmlChunk.nbsp(),
+                              HtmlChunk.text(model.getName().getValue()));
+  }
+
+  public String decoratePlainText(@NotNull SNode node, @NotNull String plainText){
+    HtmlChunk modelInfo = getModelInfo(node);
+    return decoratePlainText(plainText, modelInfo);
+  }
+
+  public String decoratePlainText(@NotNull String text, @Nullable HtmlChunk model){
+    text = MPSDocumentationMarkup.CONTENT_START +
+           MPSDocumentationMarkup.MPS_PREFORMATTED_TEXT_START +
+           text +
+           MPSDocumentationMarkup.MPS_PREFORMATTED_TEXT_END +
+           MPSDocumentationMarkup.CONTENT_END;
+    text += HtmlChunk.div().setClass("bottom").child(model);
+    return text;
+  }
+
 }
