df52aed058e7d6fc2caa619910dfada56f9d6d35
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-08-25 16:25:38 +0300

    MPS-36078 "PSI element is provided on EDT" error
    As per PlatformCoreDataKeys.BGT_DATA_PROVIDER javadoc recommendation

--- IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/editor/IdeaNodeEditor.java
+++ IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/editor/IdeaNodeEditor.java
@@ -1,10 +1,12 @@
 /*
- * Copyright 2000-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.idea.core.editor;
 
 import com.intellij.ide.SelectInContext;
+import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.usages.UsageTarget;
@@ -15,7 +17,6 @@ import jetbrains.mps.ide.projectView.ProjectViewSelectInProvider;
 import jetbrains.mps.idea.core.psi.impl.MPSPsiProvider;
 import jetbrains.mps.idea.core.usages.NodeUsageTarget;
 import jetbrains.mps.project.MPSProject;
-import jetbrains.mps.project.Project;
 import jetbrains.mps.smodel.ModelAccessHelper;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.mps.openapi.model.SNode;
@@ -30,15 +31,13 @@ import java.util.stream.Collectors;
  */
 public class IdeaNodeEditor extends NodeEditor {
 
-  public IdeaNodeEditor(Project project, SNode node) {
+  public IdeaNodeEditor(MPSProject project, SNode node) {
     super(project, node);
   }
 
   @Override
   public Object getData(@NonNls String dataId) {
     if (UsageView.USAGE_TARGETS_KEY.is(dataId)) {
-      assert myProject instanceof MPSProject;
-
       SNodeActionData ad = SNodeActionData.KEY.getData(getCurrentEditorComponent());
       SNodeReference currNodeRef = ad != null && ad.isSingle() ? ad.node() : null;
 
@@ -47,25 +46,12 @@ public class IdeaNodeEditor extends NodeEditor {
       }
       return new UsageTarget[]{new NodeUsageTarget(currNodeRef, ((MPSProject) myProject).getProject())};
     }
-    if (LangDataKeys.PSI_ELEMENT_ARRAY.is(dataId)) {
-      assert myProject instanceof MPSProject;
-
-      return new ModelAccessHelper(myProject.getModelAccess()).runReadAction(() -> {
-        SNodeActionData ad = SNodeActionData.KEY.getData(getCurrentEditorComponent());
-        if (ad == null) {
-          return null;
-        }
-
-        MPSPsiProvider psiProvider = MPSPsiProvider.getInstance(((MPSProject) myProject).getProject());
-
-        if (ad.isSingle()) {
-          final PsiElement psi = psiProvider.getPsi(ad.node());
-          return psi == null ? null : new PsiElement[] { psi };
-        } else {
-          List<PsiElement> elements = ad.nodes().map(psiProvider::getPsi).dropWhile(Objects::isNull).collect(Collectors.toList());
-          return elements.toArray(PsiElement.EMPTY_ARRAY);
-        }
-      });
+    if (PlatformCoreDataKeys.BGT_DATA_PROVIDER.is(dataId)) {
+      SNodeActionData ad = SNodeActionData.KEY.getData(getCurrentEditorComponent());
+      if (ad == null) {
+        return null;
+      }
+      return (DataProvider)slowId -> getSlowData(slowId, ad);
     }
     if (SelectInContext.DATA_KEY.is(dataId)) {
       ProjectViewSelectInProvider selectInHelper = ApplicationManager.getApplication().getService(ProjectViewSelectInProvider.class);
@@ -81,5 +67,22 @@ public class IdeaNodeEditor extends NodeEditor {
     }
 
     return super.getData(dataId);
+  }
+
+  private Object getSlowData(String dataId, SNodeActionData selection) {
+    if (LangDataKeys.PSI_ELEMENT_ARRAY.is(dataId)) {
+      return new ModelAccessHelper(myProject.getModelAccess()).runReadAction(() -> {
+        MPSPsiProvider psiProvider = MPSPsiProvider.getInstance(((MPSProject) myProject).getProject());
+
+        if (selection.isSingle()) {
+          final PsiElement psi = psiProvider.getPsi(selection.node());
+          return psi == null ? null : new PsiElement[] { psi };
+        } else {
+          List<PsiElement> elements = selection.nodes().map(psiProvider::getPsi).filter(Objects::nonNull).collect(Collectors.toList());
+          return elements.toArray(PsiElement.EMPTY_ARRAY);
+        }
+      });
+    }
+    return null;
   }
 }
