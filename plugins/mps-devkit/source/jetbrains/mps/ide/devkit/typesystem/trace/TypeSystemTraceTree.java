scm-repo-mirror/jetbrains_mps

commit 143fbb8b8baf16b0e996ae4295bee4c87d6c677c

Custom data keys for typesystem.trace
Keys are unlikely of any general use.
Use SNodeReference instead of strings or SNode instances to facilitate IDEA async UI.

plugins/mps-devkit/source/jetbrains/mps/ide/devkit/typesystem/trace/TypeSystemTraceTree.java
@@ -32,10 +32,7 @@ import jetbrains.mps.openapi.editor.cells.EditorCell;
 import jetbrains.mps.openapi.editor.message.EditorMessageOwner;
 import jetbrains.mps.openapi.editor.message.SimpleEditorMessage;
 import jetbrains.mps.project.Project;
-import jetbrains.mps.smodel.ModelAccessHelper;
 import jetbrains.mps.smodel.ModelReadRunnable;
-import jetbrains.mps.util.Pair;
-import jetbrains.mps.workbench.MPSDataKeys;
 import jetbrains.mps.workbench.action.ActionUtils;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.Nullable;
@@ -58,6 +55,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+// FWIW, before 57d31730 this code was generated from a model. Perhaps, can use
+//       vcs to ease migration back to MPS world?
 public class TypeSystemTraceTree extends MPSTree implements DataProvider {
   private final Project myProject;
   private final TypecheckingContextTracker myContextTracker;
@@ -312,31 +311,29 @@ public class TypeSystemTraceTree extends MPSTree implements DataProvider {
   @Nullable
   public Object getData(@NonNls final String id) {
     MPSTreeNode currentNode = this.getCurrentNode();
-    if (currentNode instanceof TypeSystemTraceTreeNode) {
-      return new ModelAccessHelper(myProject.getModelAccess()).runReadAction(() -> _getData(id));
+    if (false == currentNode instanceof TypeSystemTraceTreeNode) {
+      return null;
     }
-    return null;
-  }
-
-  private Object _getData(String id) {
-    AbstractOperation operation = getAssociatedOperation(this.getCurrentNode());
+    AbstractOperation operation = getAssociatedOperation(currentNode);
     if (operation == null) {
       return null;
     }
+    // I wonder if TSTT.DetailsTree#getData() shall delegate here instead of duplicating.
+    //   i.e if getCurrentNode() is the same for both trees
     final SNodeReference rule = operation.getRule();
     final SNode source = operation.getSource();
-    if (MPSDataKeys.RULE_MODEL_AND_ID.is(id) && rule != null) {
-      return new Pair<>(String.valueOf(rule.getModelReference()), String.valueOf(rule.getNodeId()));
+    if (TraceDataKeys.RULE_DECLARATION.is(id) && rule != null) {
+      return rule;
     }
-    if (MPSDataKeys.SOURCE_NODE.is(id) && source != null && source.getModel() != null) {
-      return source;
+    if (TraceDataKeys.SOURCE_NODE.is(id) && source != null) {
+      return source.getReference();
     }
     return null;
   }
 
   @Override
   protected ActionGroup createPopupActionGroup(final MPSTreeNode treeNode) {
-    return ActionUtils.groupFromActions(ActionManager.getInstance().getAction("jetbrains.mps.ide.actions.GoToNode_Action"));
+    return ActionUtils.groupFromActions(ActionManager.getInstance().getAction("jetbrains.mps.ide.devkit.actions.GoToNode_Action"));
   }
 
   private void showState(final TypeSystemTraceTreeNode newNode) {
@@ -538,23 +535,23 @@ public class TypeSystemTraceTree extends MPSTree implements DataProvider {
     @Nullable
     public Object getData(@NonNls final String id) {
       MPSTreeNode currentNode = this.getCurrentNode();
-      if (currentNode instanceof TypeSystemTraceTreeNode) {
-        return new ModelAccessHelper(myProject.getModelAccess()).runReadAction(() -> _getData(id));
+      if (false == currentNode instanceof TypeSystemTraceTreeNode) {
+        return null;
       }
-      return null;
-    }
-
-    private Object _getData(String id) {
-      AbstractOperation operation = getAssociatedOperation(this.getCurrentNode());
+      AbstractOperation operation = getAssociatedOperation(currentNode);
       if (operation == null) {
         return null;
       }
+      // beware, this code has to operate w/o model read, as IDEA prefers to consult DataProviders in
+      // a thread different from the one actual actions get data values, and we shall never provide SNode
+      // instances here. Still, if we need model read to get one, e.g. if operation.getSource() is anything but
+      // field access, it's ok to ask for one here, just don't pass SNode instances outside
       final SNodeReference rule = operation.getRule();
       final SNode source = operation.getSource();
-      if (MPSDataKeys.RULE_MODEL_AND_ID.is(id) && rule != null) {
-        return new Pair<>(String.valueOf(rule.getModelReference()), String.valueOf(rule.getNodeId()));
+      if (TraceDataKeys.RULE_DECLARATION.is(id) && rule != null) {
+        return rule;
       }
-      if (MPSDataKeys.SOURCE_NODE.is(id) && source != null && source.getModel() != null) {
+      if (TraceDataKeys.SOURCE_NODE.is(id) && source != null) {
         return source;
       }
       return null;
@@ -562,8 +559,8 @@ public class TypeSystemTraceTree extends MPSTree implements DataProvider {
 
     @Override
     protected ActionGroup createPopupActionGroup(MPSTreeNode node) {
-      return ActionUtils.groupFromActions(ActionManager.getInstance().getAction("jetbrains.mps.ide.actions.GoToNode_Action"),
-          ActionManager.getInstance().getAction("jetbrains.mps.ide.actions.GoToRule_Action"));
+      return ActionUtils.groupFromActions(ActionManager.getInstance().getAction("jetbrains.mps.ide.devkit.actions.GoToNode_Action"),
+          ActionManager.getInstance().getAction("jetbrains.mps.ide.devkit.actions.GoToRule_Action"));
     }
 
     @Override

core/typesystemEngine/source/jetbrains/mps/newTypesystem/state/blocks/Block.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2011 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,8 @@ package jetbrains.mps.newTypesystem.state.blocks;
 
 import jetbrains.mps.newTypesystem.state.NodeMaps;
 import jetbrains.mps.newTypesystem.state.State;
-import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.util.Pair;
+import org.jetbrains.mps.openapi.model.SNode;
 import org.jetbrains.mps.openapi.model.SNodeReference;
 
 import java.util.List;
@@ -26,8 +26,8 @@ import java.util.Set;
 
 public abstract class Block {
   private State myState;
-  private String myNodeModel;
-  private String myNodeId;
+  private final String myNodeModel;
+  private final String myNodeId;
 
   public Block(State state, String nodeModel, String nodeId) {
     myState = state;
@@ -37,6 +37,7 @@ public abstract class Block {
 
   public Block(State state, SNodeReference node) {
     myState = state;
+    // FIXME use SNodeReference instead of 2 strings!
     myNodeModel = node == null ? null : node.getModelReference().toString();
     myNodeId = node == null ? null : node.getNodeId().toString();
   }

plugins/mps-devkit/languages/mpsdevkit/models/devkit/actions.mps
@@ -73,7 +73,6 @@
     <import index="2ymi" ref="498d89d2-c2e9-11e2-ad49-6cf049e62fe5/java:com.intellij.ide.structureView(MPS.IDEA/)" />
     <import index="c17a" ref="8865b7a8-5271-43d3-884c-6fd1d9cfdd34/java:org.jetbrains.mps.openapi.language(MPS.OpenAPI/)" />
     <import index="mk8z" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/java:jetbrains.mps.progress(MPS.Platform/)" />
-    <import index="zdei" ref="86441d7a-e194-42da-81a5-2161ec62a379/java:jetbrains.mps.ide.typesystem.trace(MPS.Workbench/)" />
     <import index="qq03" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/java:jetbrains.mps.ide.actions(MPS.Platform/)" />
     <import index="kxvg" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/java:jetbrains.mps.ide.ui.tree.module(MPS.Platform/)" />
     <import index="ngmn" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/java:jetbrains.mps.ide.findusages.view(MPS.Platform/)" />
@@ -143,7 +142,7 @@
     <import index="et5u" ref="6ed54515-acc8-4d1e-a16c-9fd6cfe951ea/java:jetbrains.mps.messages(MPS.Core/)" />
     <import index="57ty" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/java:jetbrains.mps.ide.messages(MPS.Platform/)" />
     <import index="tpcc" ref="r:00000000-0000-4000-0000-011c89590290(jetbrains.mps.lang.structure.plugin)" />
-    <import index="dush" ref="8865b7a8-5271-43d3-884c-6fd1d9cfdd34/java:org.jetbrains.mps.openapi.persistence(MPS.OpenAPI/)" />
+    <import index="q9cj" ref="cac2fef0-41a6-4fcd-923f-f893d536b2ab/java:jetbrains.mps.ide.devkit.typesystem.trace(jetbrains.mps.ide.mpsdevkit/)" />
     <import index="4hrd" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/java:jetbrains.mps.ide.vfs(MPS.Platform/)" implicit="true" />
   </imports>
   <registry>
@@ -168,6 +167,7 @@
         <property id="1215865999894" name="mnemonic" index="ngHcd" />
         <property id="1205250923097" name="caption" index="2uzpH1" />
         <property id="7458746815261976739" name="requiredAccess" index="2YLI8m" />
+        <property id="4692598989365753297" name="updateInBackground" index="1rBW0U" />
         <property id="997079742910640235" name="fillActionContext" index="1teQrl" />
         <property id="1213273179528" name="description" index="1WHSii" />
         <child id="394857668356997869" name="places" index="med8o" />
@@ -6695,7 +6695,7 @@
               </node>
             </node>
             <node concept="liA8E" id="1rFV26uVVcA" role="2OqNvi">
-              <ref role="37wK5l" to="zdei:~TypeSystemTracePanel.showTraceForNode(jetbrains.mps.project.Project,org.jetbrains.mps.openapi.model.SNode,jetbrains.mps.nodeEditor.EditorComponent)" resolve="showTraceForNode" />
+              <ref role="37wK5l" to="q9cj:~TypeSystemTracePanel.showTraceForNode(jetbrains.mps.project.Project,org.jetbrains.mps.openapi.model.SNode,jetbrains.mps.nodeEditor.EditorComponent)" resolve="showTraceForNode" />
               <node concept="37vLTw" id="1rFV26uVVHv" role="37wK5m">
                 <ref role="3cqZAo" node="1rFV26uVVxx" resolve="mpsProject" />
               </node>
@@ -6714,7 +6714,7 @@
     <node concept="2BZ0e9" id="7oAN7tz7mq4" role="2XNbBz">
       <property role="TrG5h" value="myPanel" />
       <node concept="3uibUv" id="_wnDSoneZd" role="1tU5fm">
-        <ref role="3uigEE" to="zdei:~TypeSystemTracePanel" resolve="TypeSystemTracePanel" />
+        <ref role="3uigEE" to="q9cj:~TypeSystemTracePanel" resolve="TypeSystemTracePanel" />
       </node>
       <node concept="3Tm6S6" id="7oAN7tz7mq5" role="1B3o_S" />
     </node>
@@ -6750,7 +6750,7 @@
           <node concept="37vLTI" id="7oAN7tz7mqg" role="3clFbG">
             <node concept="2ShNRf" id="7oAN7tz7mqh" role="37vLTx">
               <node concept="1pGfFk" id="7oAN7tz7mqi" role="2ShVmc">
-                <ref role="37wK5l" to="zdei:~TypeSystemTracePanel.&lt;init&gt;(jetbrains.mps.ide.tools.BaseTool)" resolve="TypeSystemTracePanel" />
+                <ref role="37wK5l" to="q9cj:~TypeSystemTracePanel.&lt;init&gt;(jetbrains.mps.ide.tools.BaseTool)" resolve="TypeSystemTracePanel" />
                 <node concept="2WthIp" id="3ufCfAugKpM" role="37wK5m" />
               </node>
             </node>
@@ -6876,7 +6876,7 @@
                                   </node>
                                 </node>
                                 <node concept="liA8E" id="6adHiaZWalw" role="2OqNvi">
-                                  <ref role="37wK5l" to="zdei:~TypeSystemTracePanel.cleanUp()" resolve="cleanUp" />
+                                  <ref role="37wK5l" to="q9cj:~TypeSystemTracePanel.cleanUp()" resolve="cleanUp" />
                                 </node>
                               </node>
                             </node>
@@ -14414,8 +14414,9 @@
     <property role="1WHSii" value="go to source node" />
     <property role="TrG5h" value="GoToNode" />
     <property role="2uzpH1" value="Go to Source Node" />
-    <property role="3GE5qa" value="trace" />
+    <property role="3GE5qa" value="typesystem" />
     <property role="72QZ$" value="true" />
+    <property role="1rBW0U" value="true" />
     <node concept="1QGGSu" id="eJiWXTZ1vG" role="3Uehp1">
       <node concept="10M0yZ" id="eJiWXTZyzw" role="3xaMm5">
         <ref role="1PxDUh" to="l7us:~MPSIcons$Nodes" resolve="Nodes" />
@@ -14424,7 +14425,7 @@
     </node>
     <node concept="1DS2jV" id="1cJSC$rgvWj" role="1NuT2Z">
       <property role="TrG5h" value="source" />
-      <ref role="1DUlNI" to="3s15:~MPSDataKeys.SOURCE_NODE" resolve="SOURCE_NODE" />
+      <ref role="1DUlNI" to="q9cj:~TraceDataKeys.SOURCE_NODE" resolve="SOURCE_NODE" />
       <node concept="1oajcY" id="7HZe2EwZDm3" role="1oa70y" />
     </node>
     <node concept="1DS2jV" id="1GFZonKxPvD" role="1NuT2Z">
@@ -14465,15 +14466,10 @@
             </node>
             <node concept="liA8E" id="1GFZonKy3mr" role="2OqNvi">
               <ref role="37wK5l" to="kz9k:~EditorNavigator.open(org.jetbrains.mps.openapi.model.SNodeReference)" resolve="open" />
-              <node concept="2OqwBi" id="1GFZonKylmj" role="37wK5m">
-                <node concept="2OqwBi" id="1GFZonKy3od" role="2Oq$k0">
-                  <node concept="2WthIp" id="1GFZonKy3og" role="2Oq$k0" />
-                  <node concept="1DTwFV" id="1GFZonKy3oi" role="2OqNvi">
-                    <ref role="2WH_rO" node="1cJSC$rgvWj" resolve="source" />
-                  </node>
-                </node>
-                <node concept="liA8E" id="1GFZonKyluy" role="2OqNvi">
-                  <ref role="37wK5l" to="mhbf:~SNode.getReference()" resolve="getReference" />
+              <node concept="2OqwBi" id="1GFZonKy3od" role="37wK5m">
+                <node concept="2WthIp" id="1GFZonKy3og" role="2Oq$k0" />
+                <node concept="1DTwFV" id="1GFZonKy3oi" role="2OqNvi">
+                  <ref role="2WH_rO" node="1cJSC$rgvWj" resolve="source" />
                 </node>
               </node>
             </node>
@@ -14487,9 +14483,9 @@
     <property role="fJN8o" value="true" />
     <property role="TrG5h" value="GoToRule" />
     <property role="2uzpH1" value="Go to Rule" />
-    <property role="3GE5qa" value="trace" />
-    <property role="1teQrl" value="true" />
+    <property role="3GE5qa" value="typesystem" />
     <property role="72QZ$" value="true" />
+    <property role="1rBW0U" value="true" />
     <node concept="1DS2jV" id="29q6pfyoTm1" role="1NuT2Z">
       <property role="TrG5h" value="project" />
       <ref role="1DUlNI" to="qq03:~MPSCommonDataKeys.MPS_PROJECT" resolve="MPS_PROJECT" />
@@ -14503,18 +14499,6 @@
     </node>
     <node concept="tnohg" id="1cJSC$rgvWD" role="tncku">
       <node concept="3clFbS" id="1cJSC$rgvWE" role="2VODD2">
-        <node concept="3cpWs8" id="29q6pfyoTC9" role="3cqZAp">
-          <node concept="3cpWsn" id="29q6pfyoTCa" role="3cpWs9">
-            <property role="TrG5h" value="pf" />
-            <node concept="3uibUv" id="29q6pfyoTC8" role="1tU5fm">
-              <ref role="3uigEE" to="dush:~PersistenceFacade" resolve="PersistenceFacade" />
-            </node>
-            <node concept="2YIFZM" id="29q6pfyoTCb" role="33vP2m">
-              <ref role="1Pybhc" to="dush:~PersistenceFacade" resolve="PersistenceFacade" />
-              <ref role="37wK5l" to="dush:~PersistenceFacade.getInstance()" resolve="getInstance" />
-            </node>
-          </node>
-        </node>
         <node concept="3clFbF" id="29q6pfyoUd2" role="3cqZAp">
           <node concept="2OqwBi" id="29q6pfyp2Xf" role="3clFbG">
             <node concept="2OqwBi" id="29q6pfyp2OA" role="2Oq$k0">
@@ -14538,47 +14522,10 @@
             </node>
             <node concept="liA8E" id="29q6pfyp33a" role="2OqNvi">
               <ref role="37wK5l" to="kz9k:~EditorNavigator.open(org.jetbrains.mps.openapi.model.SNodeReference)" resolve="open" />
-              <node concept="2ShNRf" id="29q6pfyp34u" role="37wK5m">
-                <node concept="1pGfFk" id="29q6pfyp53L" role="2ShVmc">
-                  <ref role="37wK5l" to="w1kc:~SNodePointer.&lt;init&gt;(org.jetbrains.mps.openapi.model.SModelReference,org.jetbrains.mps.openapi.model.SNodeId)" resolve="SNodePointer" />
-                  <node concept="2OqwBi" id="29q6pfyoTF4" role="37wK5m">
-                    <node concept="37vLTw" id="29q6pfyoTCc" role="2Oq$k0">
-                      <ref role="3cqZAo" node="29q6pfyoTCa" resolve="pf" />
-                    </node>
-                    <node concept="liA8E" id="29q6pfyoTIv" role="2OqNvi">
-                      <ref role="37wK5l" to="dush:~PersistenceFacade.createModelReference(java.lang.String)" resolve="createModelReference" />
-                      <node concept="2OqwBi" id="29q6pfyoTPD" role="37wK5m">
-                        <node concept="2OqwBi" id="29q6pfyoTJc" role="2Oq$k0">
-                          <node concept="2WthIp" id="29q6pfyoTJf" role="2Oq$k0" />
-                          <node concept="1DTwFV" id="29q6pfyoTJh" role="2OqNvi">
-                            <ref role="2WH_rO" node="1cJSC$rgvWO" resolve="ruleModelAndId" />
-                          </node>
-                        </node>
-                        <node concept="2OwXpG" id="29q6pfyoTUC" role="2OqNvi">
-                          <ref role="2Oxat5" to="18ew:~Pair.o1" resolve="o1" />
-                        </node>
-                      </node>
-                    </node>
-                  </node>
-                  <node concept="2OqwBi" id="29q6pfyoTW6" role="37wK5m">
-                    <node concept="37vLTw" id="29q6pfyoTW7" role="2Oq$k0">
-                      <ref role="3cqZAo" node="29q6pfyoTCa" resolve="pf" />
-                    </node>
-                    <node concept="liA8E" id="29q6pfyoTW8" role="2OqNvi">
-                      <ref role="37wK5l" to="dush:~PersistenceFacade.createNodeId(java.lang.String)" resolve="createNodeId" />
-                      <node concept="2OqwBi" id="29q6pfyoTW9" role="37wK5m">
-                        <node concept="2OqwBi" id="29q6pfyoTWa" role="2Oq$k0">
-                          <node concept="2WthIp" id="29q6pfyoTWb" role="2Oq$k0" />
-                          <node concept="1DTwFV" id="29q6pfyoTWc" role="2OqNvi">
-                            <ref role="2WH_rO" node="1cJSC$rgvWO" resolve="ruleModelAndId" />
-                          </node>
-                        </node>
-                        <node concept="2OwXpG" id="29q6pfyoU3Y" role="2OqNvi">
-                          <ref role="2Oxat5" to="18ew:~Pair.o2" resolve="o2" />
-                        </node>
-                      </node>
-                    </node>
-                  </node>
+              <node concept="2OqwBi" id="45WYYPkGrEb" role="37wK5m">
+                <node concept="2WthIp" id="45WYYPkGrEe" role="2Oq$k0" />
+                <node concept="1DTwFV" id="45WYYPkGrEg" role="2OqNvi">
+                  <ref role="2WH_rO" node="1cJSC$rgvWO" resolve="rule" />
                 </node>
               </node>
             </node>
@@ -14587,14 +14534,14 @@
       </node>
     </node>
     <node concept="1DS2jV" id="1cJSC$rgvWO" role="1NuT2Z">
-      <property role="TrG5h" value="ruleModelAndId" />
-      <ref role="1DUlNI" to="3s15:~MPSDataKeys.RULE_MODEL_AND_ID" resolve="RULE_MODEL_AND_ID" />
+      <property role="TrG5h" value="rule" />
+      <ref role="1DUlNI" to="q9cj:~TraceDataKeys.RULE_DECLARATION" resolve="RULE_DECLARATION" />
       <node concept="1oajcY" id="7HZe2EwZDis" role="1oa70y" />
     </node>
   </node>
   <node concept="Zd50a" id="1cJSC$rgvWQ">
     <property role="TrG5h" value="TraceActions" />
-    <property role="3GE5qa" value="trace" />
+    <property role="3GE5qa" value="typesystem" />
     <node concept="Zd509" id="1cJSC$rgvWR" role="Zd508">
       <ref role="1bYAoF" node="1cJSC$rgvWh" resolve="GoToNode" />
       <node concept="pLAjd" id="1cJSC$rgvWS" role="Zd501">

plugins/mps-devkit/languages/mpsdevkit/source_gen.caches/jetbrains/mps/ide/devkit/actions/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="1i3azdlsshnjll45ciuedx0z3olbrrv">
+<product version="3" modelHash="-5bcpdd0bhycr6spj8ejb4pe8n38awat">
   <files names="AccessoriesGroupActions_ActionGroup.java:AddAccessoryModel_Action.java:BehaviorCodeOverrideImplementMenuGroup_ActionGroup.java:CalcSNodeStatistic_Action.java:CellProperties_Action.java:Contribute2ToolsInternalA_ActionGroup.java:Contribute2ToolsInternalC_ActionGroup.java:ContributeModuleUsage_ActionGroup.java:CreateDefaultEditor_Action.java:DefaultForGNOME_KeymapChanges.java:Default_KeymapChanges.java:DeleteGenerator_Action.java:DevKitTools_ActionGroup.java:EditorInternalEx_ActionGroup.java:EmulateMakeSession_Action.java:FindLanguageConceptsUsages_Action.java:FindLanguageUsages_ActionGroup.java:FindModuleUsage_Action.java:FindRootableConceptsWithoutIcons_Action.java:FindUnusedAndDeprecatedConcepts_Action.java:GenerateEditorPopup_ActionGroup.java:GenerationTraceActions_ActionGroup.java:GeneratorActions_Delete_ActionGroup.java:GoToConceptDeclaration_Action.java:GoToEditorDeclarationHelper.java:GoToEditorDeclaration_Action.java:GoToNode_Action.java:GoToRule_Action.java:GoToRules_Action.java:GoToUsageInMappingConfig_Action.java:GotoConceptAspect_ActionGroup.java:HighlightCellDependencies_Action.java:ImplementBehaviorMethod_Action.java:IntentionsGroup_ActionGroup.java:InternalReactivateEDTExecutorQueue_Action.java:LanguageActionsEx_ActionGroup.java:LanguageHierarchy_Action.java:LanguageNewActionsEx_ActionGroup.java:LanguageNewCustomPartActions_ActionGroup.java:LanguageNewGenerator_ActionGroup.java:MacOSX105_KeymapChanges.java:Mac_KeymapChanges.java:MockMakeSessionProvider.java:Mpsdevkit_ApplicationPlugin.java:Mpsdevkit_ProjectPlugin.java:NamespaceNewActionsEx_ActionGroup.java:NavigateToGeneratedCode_Action.java:NewAccessoryModel_Action.java:NewAspectModelActionHelper.java:NewAspectModelByDescriptor_Action.java:NewDevKit_Action.java:NewGenerator_Action.java:NewLanguage_Action.java:NewRuntimeModule_Action.java:NodeExplorer_Tool.java:OverrideBehaviorMethod_Action.java:PrintNodeID_Action.java:ProjectNewActionsEx_ActionGroup.java:PullGeneratorUpFromLanguage_Action.java:PullGeneratorUp_Contrib_ActionGroup.java:ReloadAll_Action.java:RuntimeFolderActions_ActionGroup.java:ShowCellInExplorer_Action.java:ShowEditorMenuItemTrace_Action.java:ShowGenerationTrace_Action.java:ShowGenerationTraceback_Action.java:ShowNodeInExplorer_Action.java:ShowNodeIn_ActionGroup.java:ShowOriginNode_Action.java:ShowStructure_Action.java:ShowTypeSystemTrace_Action.java:StructureAdditions_ActionGroup.java:TraceActions_ActionGroup.java:TraceActions_KeymapChanges.java:TraceTool_Tool.java:Trace_KeymapChanges.java:startup.properties" />
 </product>
 

plugins/mps-devkit/languages/mpsdevkit/source_gen/jetbrains/mps/ide/devkit/actions/GoToNode_Action.java
@@ -7,8 +7,8 @@ import javax.swing.Icon;
 import jetbrains.mps.icons.MPSIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import java.util.Map;
-import org.jetbrains.mps.openapi.model.SNode;
-import jetbrains.mps.workbench.MPSDataKeys;
+import org.jetbrains.mps.openapi.model.SNodeReference;
+import jetbrains.mps.ide.devkit.typesystem.trace.TraceDataKeys;
 import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.ide.actions.MPSCommonDataKeys;
 import org.jetbrains.annotations.NotNull;
@@ -33,7 +33,7 @@ public class GoToNode_Action extends BaseAction {
       return false;
     }
     {
-      SNode p = event.getData(MPSDataKeys.SOURCE_NODE);
+      SNodeReference p = event.getData(TraceDataKeys.SOURCE_NODE);
       if (p == null) {
         return false;
       }
@@ -48,6 +48,6 @@ public class GoToNode_Action extends BaseAction {
   }
   @Override
   public void doExecute(@NotNull final AnActionEvent event, final Map<String, Object> _params) {
-    new EditorNavigator(event.getData(MPSCommonDataKeys.MPS_PROJECT)).shallFocus(false).shallSelect(true).open(event.getData(MPSDataKeys.SOURCE_NODE).getReference());
+    new EditorNavigator(event.getData(MPSCommonDataKeys.MPS_PROJECT)).shallFocus(false).shallSelect(true).open(event.getData(TraceDataKeys.SOURCE_NODE));
   }
 }

plugins/mps-devkit/languages/mpsdevkit/source_gen/jetbrains/mps/ide/devkit/actions/GoToRule_Action.java
@@ -9,13 +9,10 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import java.util.Map;
 import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.ide.actions.MPSCommonDataKeys;
-import jetbrains.mps.internal.collections.runtime.MapSequence;
-import jetbrains.mps.util.Pair;
-import jetbrains.mps.workbench.MPSDataKeys;
+import org.jetbrains.mps.openapi.model.SNodeReference;
+import jetbrains.mps.ide.devkit.typesystem.trace.TraceDataKeys;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
 import jetbrains.mps.openapi.navigation.EditorNavigator;
-import jetbrains.mps.smodel.SNodePointer;
 
 public class GoToRule_Action extends BaseAction {
   private static final Icon ICON = MPSIcons.Nodes.Rule;
@@ -37,14 +34,12 @@ public class GoToRule_Action extends BaseAction {
     }
     {
       MPSProject p = event.getData(MPSCommonDataKeys.MPS_PROJECT);
-      MapSequence.fromMap(_params).put("project", p);
       if (p == null) {
         return false;
       }
     }
     {
-      Pair<String, String> p = event.getData(MPSDataKeys.RULE_MODEL_AND_ID);
-      MapSequence.fromMap(_params).put("ruleModelAndId", p);
+      SNodeReference p = event.getData(TraceDataKeys.RULE_DECLARATION);
       if (p == null) {
         return false;
       }
@@ -53,7 +48,6 @@ public class GoToRule_Action extends BaseAction {
   }
   @Override
   public void doExecute(@NotNull final AnActionEvent event, final Map<String, Object> _params) {
-    PersistenceFacade pf = PersistenceFacade.getInstance();
-    new EditorNavigator(((MPSProject) MapSequence.fromMap(_params).get("project"))).shallSelect(true).open(new SNodePointer(pf.createModelReference(((Pair<String, String>) MapSequence.fromMap(_params).get("ruleModelAndId")).o1), pf.createNodeId(((Pair<String, String>) MapSequence.fromMap(_params).get("ruleModelAndId")).o2)));
+    new EditorNavigator(event.getData(MPSCommonDataKeys.MPS_PROJECT)).shallSelect(true).open(event.getData(TraceDataKeys.RULE_DECLARATION));
   }
 }

jetbrains/mps

commit a261329b248f4fa7012cbdc6c1e4d05e7e7d8c45

Fix an exception introduced in 143fbb8b8baf16b0e996ae4295bee4c87d6c677c

plugins/mps-devkit/source/jetbrains/mps/ide/devkit/typesystem/trace/TypeSystemTraceTree.java
@@ -552,7 +552,7 @@ public class TypeSystemTraceTree extends MPSTree implements DataProvider {
         return rule;
       }
       if (TraceDataKeys.SOURCE_NODE.is(id) && source != null) {
-        return source;
+        return source.getReference();
       }
       return null;
     }