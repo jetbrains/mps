8d5fc24d25166d5e6d1e8e1bdc223365aa1f3ae9
Fedor Isakov <fisakov@gmail.com>
2023-09-15 13:24:00 +0200

    Enable support for multiple genplans referred to from devkits
    Two (or potentially more) genplans can be used together in a single model
    in case they constitute a single plan with forks, by means of using "fork of"
    feature.

---
+++ core/generator/source/jetbrains/mps/generator/CompositeInterpretedPlanProvider.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+package jetbrains.mps.generator;
+
+import jetbrains.mps.generator.ModelGenerationPlan.Provider;
+import jetbrains.mps.generator.impl.GenPlanTranslator;
+import jetbrains.mps.generator.impl.plan.EngagedGeneratorCollector;
+import jetbrains.mps.generator.impl.plan.RegularPlanBuilder;
+import jetbrains.mps.messages.IMessageHandler;
+import jetbrains.mps.messages.Message;
+import jetbrains.mps.messages.MessageKind;
+import jetbrains.mps.smodel.language.LanguageRegistry;
+import kotlinx.coroutines.repackaged.net.bytebuddy.implementation.bytecode.Throw;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.mps.openapi.model.SModel;
+import org.jetbrains.mps.openapi.model.SModelReference;
+import org.jetbrains.mps.openapi.model.SNode;
+import org.jetbrains.mps.openapi.model.SNodeReference;
+import org.jetbrains.mps.openapi.module.SRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * @author Fedor Isakov
+ */
+public class CompositeInterpretedPlanProvider implements ModelGenerationPlan.Provider {
+
+  private final LanguageRegistry myLanguageRegistry;
+  private final IMessageHandler myMessageHandler;
+  private final Collection<Provider> myProviders;
+
+  public CompositeInterpretedPlanProvider(LanguageRegistry languageRegistry, IMessageHandler messageHandler, Collection<Provider> providers) {
+    myLanguageRegistry = languageRegistry;
+    myMessageHandler = messageHandler;
+    myProviders = providers;
+  }
+
+
+  @Nullable
+  @Override
+  public ModelGenerationPlan getPlan(@NotNull SModel model) {
+    // mostly copied from InterpretedPlanProvider with changes reflecting multiple plan nodes
+    Collection<SNode> planNodes = collectPlanNodes(new ArrayList<>());
+    if (planNodes.isEmpty()) {
+      return null;
+    }
+    LinkedList<SNode> sortedPlanNodes = new LinkedList<>();
+    for (SNode planDecl : planNodes) {
+      myMessageHandler.handle(Message.info(InterpretedPlanProvider.class, String.format("Interpreted plan from node %s", planDecl.getPresentation()), planDecl.getReference(), null));
+
+      SNode forkOwner = GenPlanTranslator.getForkOwner(planDecl);
+      if (forkOwner != null) {
+        if (planNodes.contains(forkOwner)) {
+          myMessageHandler.handle(Message.info(InterpretedPlanProvider.class, String.format("Fork of %s", forkOwner.getPresentation()), forkOwner.getReference(), null));
+          sortedPlanNodes.addFirst(planDecl);
+        }else{
+          myMessageHandler.handle(Message.createMessage(MessageKind.ERROR, InterpretedPlanProvider.class.toString(), String.format("Fork owner not in scope %s", forkOwner.getPresentation()), forkOwner.getReference()));
+          return null;
+        }
+      } else {
+        sortedPlanNodes.add(planDecl);
+      }
+    }
+    // FIXME in fact, shall respect additional languages passed through GenerationParametersProviderEx.getAdditionalLanguages(SModel), like
+    // original GenerationPlan did. However, it's rarely (if ever) used feature and contemporary GPs replace it completely, so I do not bother.
+    EngagedGeneratorCollector egc = new EngagedGeneratorCollector(myLanguageRegistry, model);
+    RegularPlanBuilder planBuilder = new RegularPlanBuilder(myLanguageRegistry, egc.getGenerators(), myMessageHandler);
+
+    GenPlanTranslator gpt = null;
+    for (SNode planNode : sortedPlanNodes) {
+      gpt = new GenPlanTranslator(planNode);
+      gpt.feedMulti(planBuilder);
+    }
+    return planBuilder.wrapUp(gpt.getPlanIdentity());
+  }
+
+  private Collection<SNode> collectPlanNodes(Collection<SNode> planNodes) {
+    for (Provider provider : myProviders) {
+      if (provider instanceof InterpretedPlanProvider) {
+        SNode planNode = ((InterpretedPlanProvider) provider).getPlanNode();
+        if (planNode != null) {
+          planNodes.add(planNode);
+        }
+      }
+    }
+    return planNodes;
+  }
+
+}
