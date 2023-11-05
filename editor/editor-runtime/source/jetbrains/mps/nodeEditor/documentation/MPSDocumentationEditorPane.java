0df93d896d39ecb9d446b2a36846c00bc73365bc
tatiana.elfimova <tatiana.elfimova@jetbrains.com>
2023-11-05 15:12:12 +0100

    Process mouse events in editorPane

--- editor/editor-runtime/source/jetbrains/mps/nodeEditor/documentation/MPSDocumentationEditorPane.java
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/documentation/MPSDocumentationEditorPane.java
@@ -9,6 +9,8 @@ import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.colors.EditorColorsUtil;
 import com.intellij.openapi.editor.impl.EditorCssFontResolver;
+import com.intellij.openapi.ui.popup.JBPopup;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.ui.HTMLEditorKitBuilder;
@@ -17,10 +19,15 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JEditorPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Element;
+import javax.swing.text.StyledDocument;
 import javax.swing.text.View;
 import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
 import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 
 public final class MPSDocumentationEditorPane extends JEditorPane implements Disposable {
   private static final ColorKey COLOR_KEY = EditorColors.DOCUMENTATION_COLOR;
@@ -95,5 +102,53 @@ public final class MPSDocumentationEditorPane extends JEditorPane implements Dis
       }
     }
     return null;
+  }
+
+  private JBPopup myHint;
+  private Point myInitialPress;
+
+  public void setHint(@NotNull JBPopup hint) {
+    myHint = hint;
+    Disposer.register(hint, () -> myHint = null);
+  }
+
+  @Override
+  protected void processMouseEvent(MouseEvent e) {
+    if (e.getID() == MouseEvent.MOUSE_PRESSED && myHint != null) {
+      myInitialPress = null;
+      StyledDocument document = (StyledDocument)getDocument();
+      int x = e.getX();
+      int y = e.getY();
+      if (!hasTextAt(document, x, y) &&
+          !hasTextAt(document, x + 3, y) &&
+          !hasTextAt(document, x - 3, y) &&
+          !hasTextAt(document, x, y + 3) &&
+          !hasTextAt(document, x, y - 3)) {
+        myInitialPress = e.getPoint();
+      }
+    }
+    super.processMouseEvent(e);
+  }
+
+  private boolean hasTextAt(StyledDocument document, int x, int y) {
+    Element element = document.getCharacterElement(viewToModel2D(new Point(x, y)));
+    try {
+      String text = document.getText(element.getStartOffset(), element.getEndOffset() - element.getStartOffset());
+      return !text.trim().isEmpty();
+    }
+    catch (BadLocationException ignored) {
+      return false;
+    }
+  }
+
+  @Override
+  protected void processMouseMotionEvent(MouseEvent e) {
+    if (e.getID() == MouseEvent.MOUSE_DRAGGED && myHint != null && myInitialPress != null) {
+      Point location = myHint.getLocationOnScreen();
+      myHint.setLocation(new Point(location.x + e.getX() - myInitialPress.x, location.y + e.getY() - myInitialPress.y));
+      e.consume();
+      return;
+    }
+    super.processMouseMotionEvent(e);
   }
 }
