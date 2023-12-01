1f34bb5d05460b80fa1a9fe82e4dff508c9626a6
Fedor Isakov <fisakov@gmail.com>
2023-12-01 11:04:10 +0100

    Make sample formulaLanguage

--- samples/formulaLanguage/language/generator/source_gen/jetbrains/mps/formulaLanguage/Generator.java
+++ samples/formulaLanguage/language/generator/source_gen/jetbrains/mps/formulaLanguage/Generator.java
@@ -8,10 +8,8 @@ import jetbrains.mps.smodel.language.LanguageRuntime;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.mps.openapi.module.SModuleReference;
 import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
-import jetbrains.mps.samples.formulaLanguage.generator.baseLanguage.QueriesGenerated;
 import java.util.Collection;
 import org.jetbrains.mps.openapi.language.SLanguage;
-import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory;
 import java.util.Arrays;
 
 public class Generator extends TemplateModuleInterpreted2 {
@@ -28,14 +26,12 @@ public class Generator extends TemplateModuleInterpreted2 {
 
   @Override
   protected void fillTemplateModels(TemplateModuleInterpreted2.TemplateModels models) {
-    models.templates("r:00000000-0000-4000-0000-011c8959043d", QueriesGenerated.class);
   }
 
 
   @Override
   public Collection<SLanguage> getTargetLanguages() {
-    SLanguage[] rv = new SLanguage[1];
-    rv[0] = MetaAdapterFactory.getLanguage(0xf3061a5392264cc5L, 0xa443f952ceaf5816L, "jetbrains.mps.baseLanguage");
+    SLanguage[] rv = new SLanguage[0];
     return Arrays.asList(rv);
   }
 
