90461003668898513897b136f1a271c2dba98419
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2024-06-19 12:49:47 +0300

    BlameDialogComponent is just a factory (almost useless)
    State is not in use, BlameDialog handles credentials.

--- workbench/mps-platform/source/jetbrains/mps/ide/blame/dialog/BlameDialogComponent.java
+++ workbench/mps-platform/source/jetbrains/mps/ide/blame/dialog/BlameDialogComponent.java
@@ -16,53 +16,25 @@
 package jetbrains.mps.ide.blame.dialog;
 
 import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.RoamingType;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
-import jetbrains.mps.ide.blame.dialog.BlameDialogComponent.MyState;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.SwingUtilities;
 import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Frame;
 
-// FIXME why there's "deprecated" storage? I didn't get the idea of 43a7fe48, and there's no more
-//       MPSErrorReporterConfigurable (gone in ec8309aa). Is there still need for this storage file?
-//       Seems that BlameDialog now uses CredentialAttributesKt and doesn't need this state any more
-@State(
-    name = "CharismaBlameDialog",
-    storages = @Storage(value = "charismaBlameDialog.xml", deprecated = true, roamingType = RoamingType.DISABLED)
-)
-public final class BlameDialogComponent implements PersistentStateComponent<MyState> {
-  private MyState myDialogState = new MyState();
-
-  BlameDialogComponent() {
+public final class BlameDialogComponent {
+  static {
     setDefaultAction();
   }
 
-  private void setDefaultAction() {
+  private static void setDefaultAction() {
     final String LAST_OK_ACTION = "IdeErrorsDialog.LAST_OK_ACTION";
     final String DEFAULT_ACTION_NAME = "DEFAULT";
     String lastActionName = PropertiesComponent.getInstance().getValue(LAST_OK_ACTION);
     if (lastActionName == null) {
       PropertiesComponent.getInstance().setValue(LAST_OK_ACTION, DEFAULT_ACTION_NAME);
     }
-  }
-
-  @Override
-  public MyState getState() {
-    return myDialogState;
-  }
-
-  @Override
-  public void loadState(@NotNull MyState state) {
-    myDialogState = state;
   }
 
   public BlameDialog createDialog(Project project, Component component) {
@@ -80,46 +52,11 @@ public final class BlameDialogComponent implements PersistentStateComponent<MySt
     return result;
   }
 
+  /**
+   * Use {@link BlameDialog} directly
+   */
   public static BlameDialogComponent getInstance() {
-    return ApplicationManager.getApplication().getService(BlameDialogComponent.class);
-  }
-
-  public static class MyState {
-    private boolean myAnonymous;
-    private String myUsername;
-    private String myPassword;
-
-
-    public MyState() {
-    }
-
-    public MyState(boolean anonymous, String username) {
-      myAnonymous = anonymous;
-      myUsername = username;
-    }
-
-    public boolean isAnonymous() {
-      return myAnonymous;
-    }
-
-    public void setAnonymous(boolean anonymous) {
-      myAnonymous = anonymous;
-    }
-
-    public String getUsername() {
-      return myUsername;
-    }
-
-    public void setUsername(String username) {
-      myUsername = username;
-    }
-
-    public String getPassword() {
-      return myPassword;
-    }
-
-    public void setPassword(String password) {
-      myPassword = password;
-    }
+    // FWIW, used to be PersistentStateComponent and an App Component. Without a state, singleton doesn't make any sense
+    return new BlameDialogComponent();
   }
 }
