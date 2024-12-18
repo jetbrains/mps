jetbrains/mps

commit 7cab2f3a5e72091d4cccc9cd81f2e304c04d8667

Introduce generationTarget parameter to genplan
The optional parameter `generationTarget` can be specified for a fork.
The parameter's value corresponds to a module facet type.
It is handled by `GenerationSession` in a way that allows to:
  1. skip optional forks if no facet is available
  2. pass the parameter down to TextGen to help select `GenerationTargetFacet`

core/generator/source/jetbrains/mps/generator/impl/GenerationSession.java
@@ -72,10 +72,12 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.function.Function;
@@ -188,6 +190,7 @@ class GenerationSession {
       //   Logic similar to the one of GMDM (takes used languages of a model) is ok for the first round.
       myEmployedLanguages.addAll(ModelContentUtil.getUsedLanguages(myOriginalInputModel));
       ArrayList<SModel> allOutputModels = new ArrayList<>(4);
+      Map<SModelReference, String> gentargetByOutputModel = new HashMap<>();
       ttrace.push("steps");
 
       ModelTransitions transitionTrace = new ModelTransitions(); // FIXME make it optional, if there are no Checkpoint steps, do not record transitions
@@ -205,7 +208,9 @@ class GenerationSession {
       majorBranch.transitionTrace = transitionTrace;
       forkQueue.add(majorBranch);
       while (!forkQueue.isEmpty()) {
-        SModel output = processGenPlanBranch(forkQueue.removeFirst(), forkQueue, monitor);
+        PlanBranchInfo branchInfo = forkQueue.removeFirst();
+        SModel output = processGenPlanBranch(branchInfo, forkQueue, monitor);
+        gentargetByOutputModel.put(output.getReference(), branchInfo.generationTarget);
         // for *each* completed GP branch, keep model as it's the outcome we are going to process further
         if (output != null) {
           allOutputModels.add(output);
@@ -223,7 +228,7 @@ class GenerationSession {
       // XXX we could use GenerationDependencies to pass more information about actual generators/languages involved (including their runtimes
       //     to facilitate proper classpath calculation
       final GenerationDependencies genDeps = new GenerationDependencies(myOriginalInputModel, myControlEnv.getOptions().getParametersProvider());
-      GenerationStatus generationStatus = new GenerationStatus(myOriginalInputModel, allOutputModels, genDeps, myLogger.getErrorCount() > 0);
+      GenerationStatus generationStatus = new GenerationStatus(myOriginalInputModel, allOutputModels, genDeps, myLogger.getErrorCount() > 0, gentargetByOutputModel);
       generationStatus.setCrossModelEnvironment(myControlEnv.getCrossModelEnvironment());
       generationStatus.setEmployedLanguages(myEmployedLanguages);
       return generationStatus;
@@ -372,6 +377,14 @@ class GenerationSession {
         lastBigTransformStepMappings.clear();
       } else if (planStep instanceof Fork) {
         Fork forkStep = (Fork) planStep;
+        String generationTarget = forkStep.getGenerationTarget();
+        // proceed with this fork if either:
+        //  - generation target is undefined
+        //  - otherwise, the original model's module has a facet of the corresponding type
+        // this saves the efforts of running generation on an optional fork
+        if (generationTarget != null && (myOriginalInputModel.getModule().getFacetOfType(generationTarget) == null)) {
+          continue;
+        }
         PlanBranchInfo bi = new PlanBranchInfo(++myBranchCounter);
         // Pair cloneTransient/changeModelReference deserves a dedicated utility.
         // Use of bi.serial is to ensure input model shows up under a proper group of models. It might be modified in-place, therefore it has to be part
@@ -385,6 +398,7 @@ class GenerationSession {
         bi.actualStateCopyOfLastBitTransformStepMappings = new ArrayList<>(lastBigTransformStepMappings);
         // bi.inputModel, clone of currInputModel, already has ORIGIN_TRACE values properly set, no need to do anything in fork().
         bi.transitionTrace = transitionTrace.fork();
+        bi.generationTarget = generationTarget;
         forkQueue.add(bi);
       }
     }

core/generator/source/jetbrains/mps/generator/GenerationPlanBuilder.java
@@ -135,6 +135,14 @@ public interface GenerationPlanBuilder {
   GenerationPlanBuilder fork();
 
   /**
+   * Sets the generation target of a {@link #fork()}. Has no effect otherwise.
+   * @param targetHint generation target
+   */
+  default void setGenerationTarget(String targetHint) {
+    // NOP by default
+  }
+
+  /**
    * Completes {@link ModelGenerationPlan} instance with any state information build is aware of (e.g. build extends relation between
    * generators for {@link #applyGeneratorWithExtended(SModule...) or respect priority rules of generators involved}
    *

core/generator/source/jetbrains/mps/generator/GenerationStatus.java
@@ -23,10 +23,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.mps.openapi.language.SLanguage;
 import org.jetbrains.mps.openapi.model.SModel;
+import org.jetbrains.mps.openapi.model.SModelReference;
 import org.jetbrains.mps.openapi.module.SRepository;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Artem Tikhomirov
@@ -40,6 +42,7 @@ public final class GenerationStatus implements IStatus {
   private final SModel myInputModel;
   // we initialize it the moment GS is created assuming we can read the input model at this time, so I don't bother with model RA.
   private final SRepository myInputModelRepo;
+  private Map<SModelReference, String> myGentargetByOutput = Collections.emptyMap();
   private GenerationDependencies myDependencies;
 
   // XXX would be great to hide this one behind a factory method, boolean errors is gross.
@@ -61,6 +64,11 @@ public final class GenerationStatus implements IStatus {
     myInputModelRepo = inputModel.getRepository();
   }
 
+  public GenerationStatus(@NotNull SModel inputModel, @NotNull Collection<SModel> outputModels, GenerationDependencies dependencies, boolean errors, Map<SModelReference, String> gentargetByOutput) {
+    this(inputModel, outputModels, dependencies, errors);
+    myGentargetByOutput = gentargetByOutput;
+  }
+
 
     @Override
   public Code getCode() {
@@ -73,6 +81,10 @@ public final class GenerationStatus implements IStatus {
   @Nullable
   public SModel getOutputModel() {
     return myOutputModel;
+  }
+
+  public String getGenerationTarget(SModelReference outputModel) {
+    return myGentargetByOutput.get(outputModel);
   }
 
   public SModel getInputModel() {

core/generator/source/jetbrains/mps/generator/ModelGenerationPlan.java
@@ -154,15 +154,24 @@ public interface ModelGenerationPlan {
 
   final class Fork implements Step {
     private final List<Step> myBranch;
+    private String myGenerationTarget;
 
     public Fork(List<Step> branch) {
       myBranch = branch;
+    }
+
+    public Fork(List<Step> branch, String generationTarget) {
+      this(branch);
+      myGenerationTarget = generationTarget;
     }
 
     public List<Step> getBranch() {
       return myBranch;
     }
 
+    public String getGenerationTarget() {
+      return myGenerationTarget;
+    }
   }
 
   /**

core/generator/source/jetbrains/mps/generator/impl/PlanBranchInfo.java
@@ -30,6 +30,7 @@ import java.util.List;
   int majorStepAtFork;
   int minorStepAtFork;
   List<Step> branch;
+  String generationTarget;
 
   // just an identifier of a branch for later reference (e.g. distinguish models of different branches in UI)
   final int serial;

core/generator/source/jetbrains/mps/generator/impl/plan/RegularPlanBuilder.java
@@ -304,6 +304,10 @@ public class RegularPlanBuilder implements GenerationPlanBuilder {
     final ForkEntry forkStep = new ForkEntry();
     mySteps.add(forkStep);
     return new RegularPlanBuilder(myLanguageRegistry, myEngagedGenerators, myMessageHandler) {
+      @Override
+      public void setGenerationTarget(String targetHint) {
+        forkStep.myGenerationTarget = targetHint;
+      }
       @NotNull
       @Override
       public ModelGenerationPlan wrapUp(@NotNull PlanIdentity planIdentity) {
@@ -559,6 +563,7 @@ public class RegularPlanBuilder implements GenerationPlanBuilder {
 
   private static class ForkEntry implements StepEntry {
     private List<StepEntry> mySteps = Collections.emptyList();
+    private String myGenerationTarget = null;
 
     public void steps(List<StepEntry> steps) {
       assert !steps.contains(this) : "Fork step shall not include itself";
@@ -584,7 +589,7 @@ public class RegularPlanBuilder implements GenerationPlanBuilder {
     public void createStep(List<Step> steps) {
       final ArrayList<Step> branch = new ArrayList<>();
       mySteps.forEach(s -> s.createStep(branch));
-      steps.add(new Fork(branch));
+      steps.add(new Fork(branch, myGenerationTarget));
     }
   }
 }

commit 6185069a959f9f03b86597300051e6e91823adfd

MPS-37409 bad GP after project migration
NPE itself is just a defect introduced in 7cab2f3a. Surfaced due to the fact there were only 'fork' steps in the erroneously cached GP.
Caching GP on a facet level was completely wrong. First, it didn't reflect the fact model being generated could get changed (which affects engaged languages and generators).
Runtime/deployment state could get changed as well (and there's no way for a module facet to find out).
GenPlanExtractor, however, is better as it's instantiated and discarded per specific and limited user activity.

core/generator/source/jetbrains/mps/generator/CustomGenerationModuleFacet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2023 JetBrains s.r.o.
+ * Copyright 2003-2024 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,8 +36,6 @@ import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
 public class CustomGenerationModuleFacet extends ModuleFacetBase implements ModelGenerationPlan.Provider {
   public static final String FACET_TYPE = "generator";
   private SModelReference myPlanModel;
-  private ModelGenerationPlan myCachedPlanInstance;
-  private long myCachedPlanTimestamp;
 
   public CustomGenerationModuleFacet(@NotNull SModule module) {
     super(FACET_TYPE, module);
@@ -53,13 +51,6 @@ public class CustomGenerationModuleFacet extends ModuleFacetBase implements Mode
       return null;
     }
 
-    final long modelActualTimestamp = planModel.getSource().getTimestamp();
-    if (myCachedPlanInstance != null && myCachedPlanTimestamp == modelActualTimestamp) {
-      // as long as there's single plan per module, no need to create MGP instance for each model, reuse.
-      return myCachedPlanInstance;
-    }
-    myCachedPlanTimestamp = modelActualTimestamp;
-
     GenPlanTranslator gpt = GenPlanTranslator.fromGenPlanModel(planModel);
     if (gpt == null) {
       Logger.getLogger(getClass()).warning(String.format("No genplan declaration found in the model %s", myPlanModel.getName()), myPlanModel);
@@ -69,8 +60,7 @@ public class CustomGenerationModuleFacet extends ModuleFacetBase implements Mode
     EngagedGeneratorCollector egc = new EngagedGeneratorCollector(languageRegistry, model); // see comment in GenPlanExtractor regarding additional languages
     RegularPlanBuilder planBuilder = new RegularPlanBuilder(languageRegistry, egc.getGenerators());
     gpt.feed(planBuilder);
-    myCachedPlanInstance = planBuilder.wrapUp(gpt.getPlanIdentity());
-    return myCachedPlanInstance;
+    return planBuilder.wrapUp(gpt.getPlanIdentity());
   }
 
   // despite public, these methods are not part of the contract.
@@ -83,7 +73,6 @@ public class CustomGenerationModuleFacet extends ModuleFacetBase implements Mode
 
   public void setPlanModelReference(@Nullable SModelReference modelRef) {
     myPlanModel = modelRef;
-    myCachedPlanInstance = null;
   }
 
   @Override

core/generator/source/jetbrains/mps/generator/GenPlanExtractor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2020 JetBrains s.r.o.
+ * Copyright 2003-2024 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package jetbrains.mps.generator;
 
 import jetbrains.mps.generator.GenerationOptions.OptionsBuilder;
-import jetbrains.mps.generator.ModelGenerationPlan.Provider;
 import jetbrains.mps.messages.IMessageHandler;
 import jetbrains.mps.messages.Message;
 import jetbrains.mps.messages.MessageKind;
@@ -35,15 +34,16 @@ import org.jetbrains.mps.openapi.module.SRepository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * For a given model, figure out generation plan associated either with module's custom facet or through devkit
  * and populate generator options appropriately.
- * @implNote doesn't address model read. may cache information about plans found
+ * @implNote Implementation doesn't address model read.
+ *           May cache information about plans found.
+ *           Note, caching not necessarily respects model changes, plan changes or language/generator deployment state.
  * @author Artem Tikhomirov
  * @since 3.4
  */
@@ -55,6 +55,9 @@ public final class GenPlanExtractor implements ModelGenerationPlan.Provider {
   // null value indicates there's no plan associated with devkit (or the plan couldn't get instantiated).
   private final Map<SModuleReference, PlanProviderInfo> myDevkitToPlan = new HashMap<>();
   private final IMessageHandler myMessageHandler;
+
+  // cache GP, separately per the method obtained
+  private final IdentityHashMap<SModel, ModelGenerationPlan> myFacetPlans = new IdentityHashMap<>(), myDevkitPlans = new IdentityHashMap<>();
 
   public GenPlanExtractor(@NotNull SRepository repository, @Nullable IMessageHandler messageHandler) {
     myRepository = repository;
@@ -79,6 +82,9 @@ public final class GenPlanExtractor implements ModelGenerationPlan.Provider {
 
   @Nullable
   private ModelGenerationPlan planFromCustomFacet(SModel model) {
+    if (myFacetPlans.containsKey(model)) {
+      return myFacetPlans.get(model);
+    }
     final SModule ownerModule = model.getModule();
     final ModelGenerationPlan.Provider facet = myOwnerModuleToFacet.get(ownerModule);
     if (facet != null) {
@@ -90,7 +96,11 @@ public final class GenPlanExtractor implements ModelGenerationPlan.Provider {
       if (f != null) {
         myMessageHandler.handle(Message.info(GenPlanExtractor.class, String.format("Module %s has facet that provides generation plans", ownerModule.getModuleName()), ownerModule.getModuleReference(), null));
         myOwnerModuleToFacet.put(ownerModule, f);
-        return f.getPlan(model);
+        ModelGenerationPlan plan = f.getPlan(model);
+        if (plan != null) {
+          myFacetPlans.put(model, plan);
+        }
+        return plan;
       } else {
         myOwnerModulesNoCustomFacet.add(ownerModule);
         // fall-through
@@ -116,6 +126,19 @@ public final class GenPlanExtractor implements ModelGenerationPlan.Provider {
    */
   @Nullable
   private ModelGenerationPlan planFromDevKit(SModel model) {
+    ModelGenerationPlan plan = myDevkitPlans.get(model);
+    if (plan != null) {
+      return plan;
+    }
+    plan = planFromDevKitImpl(model);
+    if (plan != null) {
+      myDevkitPlans.put(model, plan);
+    }
+    return plan;
+  }
+
+  @Nullable
+  private ModelGenerationPlan planFromDevKitImpl(SModel model) {
     // plans associated directly with devkit property has higher precedence than plans coming from DevKit's facets plan providers
     ArrayList<ModelGenerationPlan.Provider> facetAssociatedPlan = new ArrayList<>();
     ArrayList<ModelGenerationPlan.Provider> directPlan = new ArrayList<>();
@@ -166,7 +189,6 @@ public final class GenPlanExtractor implements ModelGenerationPlan.Provider {
           new CompositeInterpretedPlanProvider(LanguageRegistry.getInstance(myRepository), myMessageHandler, directPlan);
       return planProvider.getPlan(model);
     }
-    //noinspection LoopStatementThatDoesntLoop
     for (ModelGenerationPlan.Provider p : facetAssociatedPlan) {
       // we can get here only if there's no GP directly associated with any imported devkit
       return p.getPlan(model);
@@ -212,8 +234,8 @@ public final class GenPlanExtractor implements ModelGenerationPlan.Provider {
     myOptions.customPlan(model, p);
   }
 
-  final class PlanProviderInfo {
-    final boolean isDirect; // true if MGP is assocated with a devkit directly, false if comes through facets
+  static final class PlanProviderInfo {
+    final boolean isDirect; // true if MGP is associated with a devkit directly, false if comes through facets
     final ModelGenerationPlan.Provider provider;
 
     PlanProviderInfo(ModelGenerationPlan.Provider p, boolean direct) {

core/generator/source/jetbrains/mps/generator/impl/GenerationSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2023 JetBrains s.r.o.
+ * Copyright 2003-2024 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -210,11 +210,11 @@ class GenerationSession {
       while (!forkQueue.isEmpty()) {
         PlanBranchInfo branchInfo = forkQueue.removeFirst();
         SModel output = processGenPlanBranch(branchInfo, forkQueue, monitor);
-        gentargetByOutputModel.put(output.getReference(), branchInfo.generationTarget);
         // for *each* completed GP branch, keep model as it's the outcome we are going to process further
         if (output != null) {
           allOutputModels.add(output);
           mySessionContext.getModule().addModelToKeep(output.getReference(), true);
+          gentargetByOutputModel.put(output.getReference(), branchInfo.generationTarget);
         }
       }
 