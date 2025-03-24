0f5b826421075cfa81846e488cca0a7ea9a64b89
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2025-03-24 18:54:05 +0200

    make sure resolve info is up to date when saving a model
    there's no clear understanding in the team why it was turned off (with explicit migration step to re-save all models). I believe as long as we're changing a model, we have to keep it as much up to date as possible.

--- core/kernel/source/jetbrains/mps/smodel/SModelRepository.java
+++ core/kernel/source/jetbrains/mps/smodel/SModelRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2024 JetBrains s.r.o.
+ * Copyright 2003-2025 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,6 +111,11 @@ public class SModelRepository {
           emd.save(new SaveOptions() {
             @Override
             public boolean refreshDataSource() {
+              return true;
+            }
+
+            @Override
+            public boolean updateResolveInfoInRefs() {
               return true;
             }
           });
