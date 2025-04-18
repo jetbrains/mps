0a6970cad201bc3325575aebd3c8fd04a32546c0
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2025-04-18 19:04:22 +0300

    MPS-38367 prevent lazy model loading for diff-renamed model
    Diff renames a detached model (the one with disk content), and subsequent model traversal attempt to load model data with model reference not matching the renamed one

--- platform/vfs-tracking/source/jetbrains/mps/vfs/tracking/ConflictResolverImpl.java
+++ platform/vfs-tracking/source/jetbrains/mps/vfs/tracking/ConflictResolverImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2024 JetBrains s.r.o.
+ * Copyright 2003-2025 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -215,6 +215,8 @@ public final class ConflictResolverImpl implements StorageMemoryConflictResolver
     try {
       if (source.exists()) {
         onDisk = myPersistenceFacade.getModelFactory(source.getType()).load(source);
+        // make sure there are no subsequent attempts to alter the model's content (e.g. in case DS type supports partial/lazy loading)
+        onDisk.load();
       }
     } catch (UnsupportedDataSourceException | ModelLoadException e) {
       // properly reflect this case
