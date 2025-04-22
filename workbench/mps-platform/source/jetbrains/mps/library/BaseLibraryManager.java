jetbrains/mps

commit 3e4daba558c1ba16bbafb38311292eff175175c3

LibraryManagers as services and listeners
instead of app/project components. Helps to reduce number of cons dependencies of StandaloneMPSProject by 1.
Nevertheless, it's good to have on-demand services instead of always-present components.
Besides, -1 FS.getInstance()

workbench/mps-platform/source/jetbrains/mps/library/BaseLibraryManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2022 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@
  */
 package jetbrains.mps.library;
 
-import com.intellij.openapi.components.BaseComponent;
 import com.intellij.openapi.components.PersistentStateComponent;
+import jetbrains.mps.core.tool.environment.util.SetLibraryContributor;
 import jetbrains.mps.ide.MPSCoreComponents;
 import jetbrains.mps.library.BaseLibraryManager.LibraryState;
 import jetbrains.mps.library.contributor.LibDescriptor;
@@ -24,12 +24,11 @@ import jetbrains.mps.library.contributor.LibraryContributor;
 import jetbrains.mps.logging.Logger;
 import jetbrains.mps.util.MacroHelper;
 import jetbrains.mps.util.MacrosFactory;
-import jetbrains.mps.vfs.FileSystem;
 import jetbrains.mps.vfs.IFile;
+import jetbrains.mps.vfs.IFileSystem;
 import jetbrains.mps.vfs.util.PathFormatChecker.PathFormatException;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -37,46 +36,23 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Matcher;
 
-public abstract class BaseLibraryManager implements BaseComponent, PersistentStateComponent<LibraryState>, LibraryContributor {
-  private final LibraryInitializer myLibraryInitializer;
+public abstract class BaseLibraryManager implements PersistentStateComponent<LibraryState> {
 
-  public BaseLibraryManager() {
-    myLibraryInitializer = MPSCoreComponents.getInstance().getLibraryInitializer();
-  }
-
-  @Override
-  public boolean hiddenLanguages() {
-    return false;
-  }
-
-  @Override
-  public void initComponent() {
-    myLibraryInitializer.load(Collections.singletonList(this));
-  }
-
-  @Override
-  public void disposeComponent() {
-    myLibraryInitializer.unload(Collections.singletonList(this));
-  }
-
-  /**
-   *
-   * @return instance that deals with library initialization
-   */
-  public LibraryInitializer getInitializer() {
-    return myLibraryInitializer;
+  protected BaseLibraryManager() {
   }
 
   //-------libraries
 
-  @Override
-  public final Set<LibDescriptor> getPaths() {
+  @NotNull
+  public LibraryContributor createContributor(final IFileSystem fs) {
+    // XXX can I pass MacroHelper or similar here for greater control?
+    // XXX also would be great to keep names of original macros not to overwrite when few macros resolve to same location
     Set<LibDescriptor> result = new HashSet<>();
     for (Library lib : getUILibraries()) {
       String path = lib.getPath();
       if (path != null) {
         try {
-          IFile file = FileSystem.getInstance().getFile(path);
+          IFile file = fs.getFile(path);
           // Guess, the idea here is to make project/app contributed modules always visible (hiddenLanguages() == false)
           // Don't want to contradict at the moment, hence explicit 'false'.
           result.add(new LibDescriptor(file, null, lib.getName(), false));
@@ -91,7 +67,7 @@ public abstract class BaseLibraryManager implements BaseComponent, PersistentSta
         }
       }
     }
-    return result;
+    return SetLibraryContributor.fromSet(getClass().getSimpleName(), result);
   }
 
   public Library addLibrary(@NotNull String name) {
@@ -154,12 +130,7 @@ public abstract class BaseLibraryManager implements BaseComponent, PersistentSta
   @Override
   public void loadState(@NotNull LibraryState state) {
     myLibraryState = removeMacros(state);
-    myLibraryInitializer.update();
-  }
-
-  @Override
-  public String toString() {
-    return "BaseLibraryManager";
+    MPSCoreComponents.getInstance().getLibraryInitializer().update();
   }
 
   static class LibraryState {

workbench/mps-platform/source/jetbrains/mps/ide/library/LibraryManagerPreferences.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2017 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,26 +17,16 @@ package jetbrains.mps.ide.library;
 
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.components.JBList;
+import jetbrains.mps.ide.MPSCoreComponents;
 import jetbrains.mps.library.BaseLibraryManager;
 import jetbrains.mps.library.Library;
 import jetbrains.mps.util.ToStringComparator;
 
-import javax.swing.AbstractAction;
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.DefaultListModel;
-import javax.swing.Icon;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.Icon;
@@ -49,10 +39,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LibraryManagerPreferences {
-  private BaseLibraryManager myManager;
-  private JPanel myMainPanel = new JPanel(new BorderLayout());
-  private DefaultListModel<Library> myListModel = new DefaultListModel<>();
-  private JList<Library> myLibrariesList = new JBList<>(myListModel);
+  private final BaseLibraryManager myManager;
+  private final JPanel myMainPanel = new JPanel(new BorderLayout());
+  private final DefaultListModel<Library> myListModel = new DefaultListModel<>();
+  private final JList<Library> myLibrariesList = new JBList<>(myListModel);
 
   private boolean myChanged;
 
@@ -95,7 +85,7 @@ public class LibraryManagerPreferences {
     }
 
     if (updateManager) {
-      myManager.getInitializer().update();
+      MPSCoreComponents.getInstance().getLibraryInitializer().update();
     }
   }
 

workbench/mps-workbench/jetbrains.mps.ide/models/watching.mps
@@ -1363,15 +1363,11 @@
               <node concept="3uibUv" id="61i8ARBr8Q0" role="1tU5fm">
                 <ref role="3uigEE" to="z1c3:~ProjectLibraryManager" resolve="ProjectLibraryManager" />
               </node>
-              <node concept="2OqwBi" id="61i8ARBr9a0" role="33vP2m">
-                <node concept="37vLTw" id="61i8ARBr9a1" role="2Oq$k0">
+              <node concept="2YIFZM" id="4$IL72MaIyK" role="33vP2m">
+                <ref role="37wK5l" to="z1c3:~ProjectLibraryManager.getInstance(com.intellij.openapi.project.Project)" resolve="getInstance" />
+                <ref role="1Pybhc" to="z1c3:~ProjectLibraryManager" resolve="ProjectLibraryManager" />
+                <node concept="37vLTw" id="4$IL72MaIyL" role="37wK5m">
                   <ref role="3cqZAo" node="5T8zXYahxm$" resolve="project" />
-                </node>
-                <node concept="liA8E" id="61i8ARBr9a2" role="2OqNvi">
-                  <ref role="37wK5l" to="1m72:~ComponentManager.getComponent(java.lang.Class)" resolve="getComponent" />
-                  <node concept="3VsKOn" id="61i8ARBr9a3" role="37wK5m">
-                    <ref role="3VsUkX" to="z1c3:~ProjectLibraryManager" resolve="ProjectLibraryManager" />
-                  </node>
                 </node>
               </node>
             </node>
@@ -1444,15 +1440,11 @@
               <node concept="3uibUv" id="61i8ARBr9EA" role="1tU5fm">
                 <ref role="3uigEE" to="z1c3:~ProjectLibraryManager" resolve="ProjectLibraryManager" />
               </node>
-              <node concept="2OqwBi" id="61i8ARBr9Ms" role="33vP2m">
-                <node concept="37vLTw" id="61i8ARBr9Mt" role="2Oq$k0">
+              <node concept="2YIFZM" id="4$IL72MaH1Z" role="33vP2m">
+                <ref role="37wK5l" to="z1c3:~ProjectLibraryManager.getInstance(com.intellij.openapi.project.Project)" resolve="getInstance" />
+                <ref role="1Pybhc" to="z1c3:~ProjectLibraryManager" resolve="ProjectLibraryManager" />
+                <node concept="37vLTw" id="4$IL72MaHR8" role="37wK5m">
                   <ref role="3cqZAo" node="5T8zXYahxmR" resolve="project" />
-                </node>
-                <node concept="liA8E" id="61i8ARBr9Mu" role="2OqNvi">
-                  <ref role="37wK5l" to="1m72:~ComponentManager.getComponent(java.lang.Class)" resolve="getComponent" />
-                  <node concept="3VsKOn" id="61i8ARBr9Mv" role="37wK5m">
-                    <ref role="3VsUkX" to="z1c3:~ProjectLibraryManager" resolve="ProjectLibraryManager" />
-                  </node>
                 </node>
               </node>
             </node>
@@ -1512,17 +1504,9 @@
                 <node concept="3uibUv" id="61i8ARBsoR$" role="1tU5fm">
                   <ref role="3uigEE" to="32g6:~AdditionalLibrariesManager" resolve="AdditionalLibrariesManager" />
                 </node>
-                <node concept="2OqwBi" id="61i8ARBswwZ" role="33vP2m">
-                  <node concept="2YIFZM" id="61i8ARBsvvc" role="2Oq$k0">
-                    <ref role="37wK5l" to="bd8o:~ApplicationManager.getApplication()" resolve="getApplication" />
-                    <ref role="1Pybhc" to="bd8o:~ApplicationManager" resolve="ApplicationManager" />
-                  </node>
-                  <node concept="liA8E" id="61i8ARBsxE4" role="2OqNvi">
-                    <ref role="37wK5l" to="1m72:~ComponentManager.getComponent(java.lang.Class)" resolve="getComponent" />
-                    <node concept="3VsKOn" id="61i8ARBslWj" role="37wK5m">
-                      <ref role="3VsUkX" to="32g6:~AdditionalLibrariesManager" resolve="AdditionalLibrariesManager" />
-                    </node>
-                  </node>
+                <node concept="2YIFZM" id="4$IL72MaFza" role="33vP2m">
+                  <ref role="37wK5l" to="32g6:~AdditionalLibrariesManager.getInstance()" resolve="getInstance" />
+                  <ref role="1Pybhc" to="32g6:~AdditionalLibrariesManager" resolve="AdditionalLibrariesManager" />
                 </node>
               </node>
             </node>

workbench/mps-workbench/jetbrains.mps.ide/source_gen.caches/jetbrains/mps/watching/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<product version="3" modelHash="-dxe19xasf4oy66b3xiafny7espe7xbo">
+<product version="3" modelHash="4kbxe66q00ayssq8qdhsolr1pvd6ydz">
   <files names="ModelFocusSynchronizer.java:WatchedRootsUpdater.java" />
 </product>
 

workbench/mps-workbench/jetbrains.mps.ide/source_gen/jetbrains/mps/watching/WatchedRootsUpdater.java
@@ -37,14 +37,14 @@ public class WatchedRootsUpdater implements Disposable {
   public static class ProjectListener implements ProjectManagerListener {
     @Override
     public void projectOpened(@NotNull Project project) {
-      ProjectLibraryManager plm = project.getComponent(ProjectLibraryManager.class);
+      ProjectLibraryManager plm = ProjectLibraryManager.getInstance(project);
       WatchedRootsUpdater wru = ApplicationManager.getApplication().getService(WatchedRootsUpdater.class);
       wru.initAppLibrariesIfNecessary();
       wru.processProjectLibraries(plm);
     }
     @Override
     public void projectClosing(@NotNull Project project) {
-      ProjectLibraryManager plm = project.getComponent(ProjectLibraryManager.class);
+      ProjectLibraryManager plm = ProjectLibraryManager.getInstance(project);
       WatchedRootsUpdater wru = ApplicationManager.getApplication().getService(WatchedRootsUpdater.class);
       wru.processProjectLibraries(plm);
     }
@@ -52,7 +52,7 @@ public class WatchedRootsUpdater implements Disposable {
 
   /*package*/ void initAppLibrariesIfNecessary() {
     if (!(myAppLibInitialized)) {
-      AdditionalLibrariesManager appLM = ApplicationManager.getApplication().getComponent(AdditionalLibrariesManager.class);
+      AdditionalLibrariesManager appLM = AdditionalLibrariesManager.getInstance();
       processLibrariesChange(appLM.getUILibraries(), myLibrariesRequests);
       myAppLibInitialized = true;
     }

workbench/mps-workbench/jetbrains.mps.ide/source_gen/jetbrains/mps/watching/trace.info
@@ -128,15 +128,15 @@
       <node id="6793838228148720944" at="30,0,31,0" concept="4" trace="myLibrariesRequests" />
       <node id="6793838228148720953" at="31,0,32,0" concept="4" trace="myProjectLibrariesRequests" />
       <node id="6940647831160957168" at="32,0,33,0" concept="4" trace="myAppLibInitialized" />
-      <node id="6940647831160787582" at="39,57,40,84" concept="8" />
-      <node id="6940647831160803564" at="40,84,41,106" concept="8" />
+      <node id="6940647831160787582" at="39,57,40,77" concept="8" />
+      <node id="6940647831160803564" at="40,77,41,106" concept="8" />
       <node id="6940647831161003037" at="41,106,42,40" concept="3" />
       <node id="6940647831160793822" at="42,40,43,39" concept="3" />
-      <node id="6940647831160790170" at="46,58,47,84" concept="8" />
-      <node id="6940647831160813579" at="47,84,48,106" concept="8" />
+      <node id="6940647831160790170" at="46,58,47,77" concept="8" />
+      <node id="6940647831160813579" at="47,77,48,106" concept="8" />
       <node id="6940647831160818386" at="48,106,49,39" concept="3" />
-      <node id="6940647831161114082" at="54,33,55,124" concept="8" />
-      <node id="6793838228148721268" at="55,124,56,74" concept="3" />
+      <node id="6940647831161114082" at="54,33,55,82" concept="8" />
+      <node id="6793838228148721268" at="55,82,56,74" concept="3" />
       <node id="6940647831161277926" at="56,74,57,33" concept="3" />
       <node id="6940647831160886294" at="60,71,61,77" concept="3" />
       <node id="6793838228148721319" at="64,131,65,90" concept="8" />

workbench/mps-workbench/source/META-INF/MPSComponents.xml
@@ -11,9 +11,6 @@
 
   <application-components>
     <component>
-      <implementation-class>jetbrains.mps.library.AdditionalLibrariesManager</implementation-class>
-    </component>
-    <component>
       <implementation-class>jetbrains.mps.smodel.ModelValidationSettings</implementation-class>
     </component>
     <component>
@@ -64,9 +61,6 @@
       <implementation-class>jetbrains.mps.ide.hierarchy.BaseLanguageHierarchyViewTool</implementation-class>
     </component>
     <component>
-      <implementation-class>jetbrains.mps.project.ProjectLibraryManager</implementation-class>
-    </component>
-    <component>
       <implementation-class>jetbrains.mps.ide.bookmark.BookmarkManager</implementation-class>
     </component>
     <component>
@@ -106,10 +100,12 @@
     <projectService serviceImplementation="jetbrains.mps.ide.editor.tabs.TabRootNodesTracker"/>
     <projectService serviceImplementation="jetbrains.mps.ide.editor.tabs.FileStatusChangeListener"/>
     <projectService serviceImplementation="jetbrains.mps.ide.bookmark.BookmarksUIComponent"/>
+    <projectService serviceImplementation="jetbrains.mps.project.ProjectLibraryManager" />
     <applicationService serviceInterface="jetbrains.mps.ide.platform.watching.FileSystemListenersContainer"
                         serviceImplementation="jetbrains.mps.ide.vfs.WatchingFileSystemListenersContainer" />
     <applicationService serviceImplementation="jetbrains.mps.ide.generator.TransientSwapOwnerComponent" />
     <applicationService serviceImplementation="jetbrains.mps.watching.WatchedRootsUpdater" />
+    <applicationService serviceImplementation="jetbrains.mps.library.AdditionalLibrariesManager"/>
 
     <notificationGroup id="jetbrains.mps.ide.make.StartupModuleMaker" displayType="STICKY_BALLOON" key="smm.group.name" bundle="jetbrains.mps.ide.make.Messages"/>
     <applicationService serviceImplementation="jetbrains.mps.ide.generator.GeneratorCacheComponent"/>
@@ -123,6 +119,8 @@
   <applicationListeners>
     <listener class="jetbrains.mps.watching.ModelFocusSynchronizer" topic="com.intellij.ide.FrameStateListener"/>
     <listener class="jetbrains.mps.watching.WatchedRootsUpdater$ProjectListener" topic="com.intellij.openapi.project.ProjectManagerListener"
+              activeInHeadlessMode="false" activeInTestMode="false"/>
+    <listener class="jetbrains.mps.project.ProjectLibraryManager$ProjectListener" topic="com.intellij.openapi.project.ProjectManagerListener"
               activeInHeadlessMode="false" activeInTestMode="false"/>
   </applicationListeners>
   <projectListeners>

workbench/mps-workbench/source/jetbrains/mps/ide/make/StartupModuleMakerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2022 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,7 +75,7 @@ public final class StartupModuleMakerImpl extends StartupModuleMaker implements 
     myMPSProject = ProjectHelper.fromIdeaProject(project);
     // ProjectLibraryManager used to be cons parameter
     @SuppressWarnings("unused")
-    final ProjectLibraryManager plm = project.getComponent(ProjectLibraryManager.class);
+    final ProjectLibraryManager plm = ProjectLibraryManager.getInstance(project);
     myComponents = ApplicationManager.getApplication().getComponent(MPSCoreComponents.class);
     DumbService.getInstance(project).queueTask(new DumbModeTask() {
       @Override

commit b570236aa280d67c363baf1ca283ce0a0132942a

MPS-38514 update libraries once their state changes
both for disk changes (loadState()), and UI-initiated changes (when LibraryInitializer.update() forced from UI action, LibraryContributor has to give new set of paths, which is not the case with SetLibraryContributor. The defect introduced in 3e4daba5)

workbench/mps-platform/source/jetbrains/mps/library/BaseLibraryManager.java
@@ -16,7 +16,7 @@
 package jetbrains.mps.library;
 
 import com.intellij.openapi.components.PersistentStateComponent;
-import jetbrains.mps.core.tool.environment.util.SetLibraryContributor;
+import jetbrains.mps.ide.MPSCoreComponents;
 import jetbrains.mps.library.BaseLibraryManager.LibraryState;
 import jetbrains.mps.library.contributor.LibDescriptor;
 import jetbrains.mps.library.contributor.LibraryContributor;
@@ -33,10 +33,37 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.stream.Collectors;
 
 public abstract class BaseLibraryManager implements PersistentStateComponent<LibraryState> {
+  private volatile boolean myContributorCreated = false;
+
+  private static class Contributor implements LibraryContributor {
+    private final String myName;
+    private final Supplier<Set<LibDescriptor>> mySource;
+
+    /*package*/Contributor(String name, Supplier<Set<LibDescriptor>> source) {
+      myName = name;
+      mySource = source;
+    }
+
+    @Override
+    public boolean hiddenLanguages() {
+      return false;
+    }
+
+    @Override
+    public Set<LibDescriptor> getPaths() {
+      return mySource.get();
+    }
+
+    @Override
+    public String toString() {
+      return "LibraryContributor " + myName;
+    }
+  }
 
   protected BaseLibraryManager() {
   }
@@ -45,29 +72,36 @@ public abstract class BaseLibraryManager implements PersistentStateComponent<Lib
 
   @NotNull
   public LibraryContributor createContributor(final IFileSystem fs) {
-    // XXX can I pass MacroHelper or similar here for greater control?
-    // XXX also would be great to keep names of original macros not to overwrite when few macros resolve to same location
-    Set<LibDescriptor> result = new HashSet<>();
-    for (Library lib : getUILibraries()) {
-      String path = lib.getPath();
-      if (path != null) {
-        try {
-          IFile file = fs.getFile(path);
-          // Guess, the idea here is to make project/app contributed modules always visible (hiddenLanguages() == false)
-          // Don't want to contradict at the moment, hence explicit 'false'.
-          result.add(new LibDescriptor(file, null, lib.getName(), false));
-        } catch (PathFormatException e) {
-          // fixme apyshkin
-          Matcher matcher = MacroHelper.MACRO_PATTERN.matcher(e.getProblemPath());
-          if (matcher.find()) {
-            Logger.getLogger(BaseLibraryManager.class).warning("Some paths might contain unknown macros, please define them in 'Path variables'");
-          } else {
-            throw e;
+    // the way library editing is perfomed, we are supposed to keep LibraryContributor and provide different set of paths, rather than change
+    // number of contributors.
+    Supplier<Set<LibDescriptor>> f = () -> {
+      // indicate there's need to reload libraries when persistent state changes
+      myContributorCreated = true;
+      // XXX can I pass MacroHelper or similar here for greater control?
+      // XXX also would be great to keep names of original macros not to overwrite when few macros resolve to same location
+      Set<LibDescriptor> result = new HashSet<>();
+      for (Library lib : getUILibraries()) {
+        String path = lib.getPath();
+        if (path != null) {
+          try {
+            IFile file = fs.getFile(path);
+            // Guess, the idea here is to make project/app contributed modules always visible (hiddenLanguages() == false)
+            // Don't want to contradict at the moment, hence explicit 'false'.
+            result.add(new LibDescriptor(file, null, lib.getName(), false));
+          } catch (PathFormatException e) {
+            // fixme apyshkin
+            Matcher matcher = MacroHelper.MACRO_PATTERN.matcher(e.getProblemPath());
+            if (matcher.find()) {
+              Logger.getLogger(BaseLibraryManager.class).warning("Some paths might contain unknown macros, please define them in 'Path variables'");
+            } else {
+              throw e;
+            }
           }
         }
       }
-    }
-    return SetLibraryContributor.fromSet(getClass().getSimpleName(), result);
+      return result;
+    };
+    return new Contributor(getClass().getSimpleName(), f);
   }
 
   public Library addLibrary(@NotNull String name) {
@@ -134,7 +168,11 @@ public abstract class BaseLibraryManager implements PersistentStateComponent<Lib
   @Override
   public void loadState(@NotNull LibraryState state) {
     myLibraryState = state;
-//    MPSCoreComponents.getInstance().getLibraryInitializer().update(); OMG - repo reload on component state init?!
+    if (myContributorCreated) {
+      // not nice, but there's no other way to figure out LibraryInitializer needs update when IDEA decides to load new state
+      // myContributorCreated condition at least avoids update() for the first state initialization
+      MPSCoreComponents.getInstance().getLibraryInitializer().update();
+    }
   }
 
   static class LibraryState {