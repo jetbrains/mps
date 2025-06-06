16709593282564e8e166d98d238c9527a5519ba6
Vaclav <vaclav.pech@gmail.com>
2025-06-06 14:10:03 +0200

    MPS-38470 - minor visual improvements, use HTML to format the content

--- editor/editor-runtime/source/jetbrains/mps/nodeEditor/NodeInformationDialog.java
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/NodeInformationDialog.java
@@ -15,22 +15,19 @@
  */
 package jetbrains.mps.nodeEditor;
 
+import com.intellij.ide.plugins.newui.EmptyCaret;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.awt.RelativePoint;
-import com.intellij.util.ui.JBUI.Borders;
+import com.intellij.util.ui.JBUI;
 import jetbrains.mps.smodel.presentation.NodePresentationUtil;
 import org.jetbrains.mps.openapi.model.SModelReference;
 import org.jetbrains.mps.openapi.model.SNode;
 import org.jetbrains.mps.openapi.model.SReference;
 
+import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-import java.awt.Color;
-import java.awt.Frame;
-import java.awt.Insets;
 import java.awt.Point;
 
 // name ...Dialog is a legacy (used to be a JDialog); didn't rename as this class is part of EditorComponent interface
@@ -45,22 +42,21 @@ public final class NodeInformationDialog {
   }
 
   public void show(String text) {
-    // COLORS: Remove hardcoded color
-    final Color BACKGROUND_COLOR = MPSColors.yellow;
-    final var textArea = new JTextArea();
+    final var textArea = new JEditorPane("text/html", "");
     textArea.setEditable(false);
-    textArea.setText(text);
-    textArea.setFont(myEditorComponent.getEditorComponentSettings().getDefaultFont());
+    textArea.setCaret(new EmptyCaret());
+    textArea.setText("<html>" + text + "</html");
+    textArea.setFont(JBUI.Fonts.label());
 
     textArea.setOpaque(true);
-    textArea.setBackground(BACKGROUND_COLOR);
-    textArea.setBorder(Borders.empty(5));
+    textArea.setBackground(JBUI.Panels.simplePanel().getBackground());
+    textArea.setBorder(JBUI.Borders.empty(5));
 
     JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(textArea);
     scrollPane.setBorder(new LineBorder(MPSColors.BLACK));
     var popup = JBPopupFactory.getInstance().createComponentPopupBuilder(scrollPane, textArea)
                               .setRequestFocus(true).setFocusable(true)
-                              .setMovable(true).setResizable(true).setShowBorder(true).setShowShadow(false)
+                              .setMovable(false).setResizable(false).setShowBorder(true).setShowShadow(false)
                               .setLocateWithinScreenBounds(true)
                               .setCancelOnClickOutside(true).setCancelKeyEnabled(true)
                               .setCancelCallback(this::dispose).createPopup();
@@ -77,41 +73,41 @@ public final class NodeInformationDialog {
   public static String createNodeInfo(final SNode node) {
     StringBuilder result = new StringBuilder();
 
-    result.append("model = ").append(node.getModel().getReference()).append('\n');
-    result.append("concept = ").append(node.getConcept().getName()).append('\n');
-    result.append("language = ").append(node.getConcept().getLanguage().getQualifiedName()).append('\n');
-    result.append("ID = ").append(node.getNodeId().toString()).append('\n');
+    result.append("<b>model</b> = ").append(node.getModel().getReference()).append("<br/>\n");
+    result.append("<b>concept</b> = ").append(node.getConcept().getName()).append("<br/>\n");
+    result.append("<b>language</b> = ").append(node.getConcept().getLanguage().getQualifiedName()).append("<br/>\n");
+    result.append("<b>ID</b> = ").append(node.getNodeId().toString()).append("<br/>\n");
 
     Iterable<? extends SReference> references = node.getReferences();
     if (references.iterator().hasNext()) {
-      result.append("\nReferences:\n");
+      result.append("\n<b>References:</b><br/>\n");
     }
     for (SReference ref : references) {
-      result.append(ref.getLink().getName()).append('\n');
+      result.append("<i>" + ref.getLink().getName() + "</i>").append("<br/>\n");
       SNode target = ref.getTargetNode();
       final SModelReference refTargetModel = ref.getTargetSModelReference();
 
       if (target == null) {
-        result.append("Bad Reference");
+        result.append("<i>Bad Reference</i>");
         if (refTargetModel != null) {
-          result.append(", target model = ").append(refTargetModel);
+          result.append(", <b>target model</b> = ").append(refTargetModel);
         }
-        result.append('\n');
+        result.append("<br/>\n");
       } else {
         if (target.getModel() != null) {
-          result.append("Model = ").append(target.getModel().getReference()).append('\n');
+          result.append("<b>Model</b> = ").append(target.getModel().getReference()).append('\n');
         } else {
-          result.append("Target node in role doesn't belong to a model");
+          result.append("<i>Target node in role doesn't belong to a model</i>");
           if (refTargetModel != null) {
-            result.append(", target model = ").append(refTargetModel);
+            result.append(", <b>target model</b> = ").append(refTargetModel);
           }
-          result.append('\n');
+          result.append("<br/>\n");
         }
-        result.append("Node = ").append(NodePresentationUtil.getPathToRoot(target)).append('\n');
-        result.append("ID = ").append(target.getNodeId().toString()).append('\n');
+        result.append("<b>Node</b> = ").append(NodePresentationUtil.getPathToRoot(target)).append('\n');
+        result.append("<b>ID</b> = ").append(target.getNodeId().toString()).append('\n');
       }
 
-      result.append('\n');
+      result.append("<br/>\n");
     }
 
     return result.toString();
