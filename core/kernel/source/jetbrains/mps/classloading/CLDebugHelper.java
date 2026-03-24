6ca78db8a2418dd3a516426dbbace0a920258f83
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2026-03-24 14:47:06 +0200

    CLM: debug printout, skip details of valid dependencies
    Generally, we investigate CL failures and are primarily interested in broken dependencies.

--- core/kernel/source/jetbrains/mps/classloading/CLDebugHelper.java
+++ core/kernel/source/jetbrains/mps/classloading/CLDebugHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * Copyright 2000-2026 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.classloading;
 
@@ -39,12 +39,15 @@ public final class CLDebugHelper {
     ClassLoaderManager clm = myProject.getComponent(ClassLoaderManager.class);
     LinkedHashSet<CModule> firstLevelDeps = new LinkedHashSet<>();
     clm.getModulesWatcher().visitDependencies(module, firstLevelDeps::add, false);
-    System.out.printf("%s: dependency graph\n", module.getModuleName());
+    System.out.printf("%s(%s): dependency graph\n", module.getModuleName(), clm.getModulesWatcher().getStatus(module));
     for (CModule dep : firstLevelDeps) {
       if (module.equals(dep.getModuleReference())) {
         continue;
       }
       System.out.printf("\t%s  --  %s\n", dep, dep.getStatus());
+      if (dep.getStatus().isValid()) {
+        continue;
+      }
       clm.getModulesWatcher().visitDependencies(dep.getModuleReference(), (d) -> {
         if (dep.getModuleReference().equals(d.getModuleReference())) {
           return;
